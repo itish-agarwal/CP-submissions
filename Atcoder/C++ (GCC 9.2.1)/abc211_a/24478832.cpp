bc872dfc00c4d6fd8902779a51f183366e7acb70
Itish Agarwal <56088263+itish-agarwal@users.noreply.github.com>
2022-06-04 15:20:16 +0530

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc211_a/24478832.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  int A, B;
+  cin >> A >> B;
+
+  cout << double(A - B)/3 + B << '\n';
+
+}
