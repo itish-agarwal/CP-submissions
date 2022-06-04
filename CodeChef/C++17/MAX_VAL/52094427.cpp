3d4540c054b90477e3421f85c1a6869ecb67c05b
Itish Agarwal <56088263+itish-agarwal@users.noreply.github.com>
2022-06-04 15:27:15 +0530

    Committing files

---
+++ CodeChef/C++17/MAX_VAL/52094427.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using namespace chrono;
+
+#ifndef ONLINE_JUDGE
+#define debug(x...) cout << "[" << #x << ": "; pr(x); cout << "]\n";
+#else
+#define debug(x...)
+#endif
+
+void pr(string x)  { cout << '\"' << x << '\"'; }
+void pr(char x)  { cout << '\'' << x << '\''; }
+void pr(long double x)  { cout << x; }
+void pr(long long x)  { cout << x; }
+void pr(double x)  { cout << x; }
+void pr(float x)  { cout << x; }
+void pr(long x)  { cout << x; }
+void pr(bool x)  { cout << x; }
+void pr(int x)  { cout << x; }
+
+template <class T, class V> void pr(const pair<T, V> &x);
+template <class T, class V> void pr(const map<T, V>& mp);
+template <class T, class... V> void pr(T t, V... v);
+template <class T> void pr(const multiset<T>& pq);
+template <class T> void pr(const vector<T>& v);
+template <class T> void pr(const set<T>& pq);
+
+template <class T, class V> void pr(const pair<T, V>& x) {
+  cout << "{"; pr(x.first); cout << ", "; pr(x.second); cout << "}";
+}
+template <class T, class V> void pr(const map<T, V>& mp) {
+  for(auto it = mp.begin(); it != mp.end(); ++it) { pr(*it); cout << " "; }
+}
+template <class T> void pr(const multiset<T>& pq) {
+  vector<T> values(pq.begin(), pq.end()); pr(values);
+}
+template <class T> void pr(const vector<T>& v) {
+  for(int i = 0; i < (int) v.size(); ++i) { pr(v[i]); cout << (i + 1 < (int) v.size() ? " " : ""); }
+}
+template <class T> void pr(const set<T>& pq) {
+  vector<T> values(pq.begin(), pq.end()); pr(values);
+}
+template <class T, class... V> void pr(T t, V... v) {
+  pr(t); if(sizeof...(v)) cout << " | ";
+  pr(v...);
+}
+//Global space begins
+const int nax = 1e5 + 7;
+
+void test_case() {
+  int N, Q;
+  cin >> N >> Q;
+
+  vector<int> cnt(nax);
+
+  for(int i = 0; i < N; ++i) {
+    int a;
+    cin >> a;
+    cnt[a] += 1;
+  }
+
+  for(int i = 1; i < nax; ++i)
+    cnt[i] += cnt[i - 1];
+
+  for(int q = 0; q < Q; ++q) {
+    int L, R;
+    cin >> L >> R;
+
+    long long X = cnt[R] - (L > 0 ? cnt[L - 1] : 0);
+    cout <<  10 * X - R + L << " ";
+  }
+
+  cout << '\n';
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  int tests = 1;
+  cin >> tests;
+
+  while(tests--) 
+    test_case();
+}
