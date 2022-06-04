18c91fd5dccfcff6395a7afd95798ffebc906400
Itish Agarwal <56088263+itish-agarwal@users.noreply.github.com>
2022-06-04 15:27:38 +0530

    Committing files

---
+++ CodeChef/C++17/SEPT203/51495708.cpp
@@ -0,0 +1,104 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using namespace chrono;
+
+#ifndef ONLINE_JUDGE
+#define debug(x...) cout << "[" << #x << ": "; pr(x); cout << "]\n";
+#else
+#define debug(x...)
+#endif
+
+void pr(string x)  { cout << '\"' << x << '\"'; }
+void pr(char x)  { cout << '\'' << x << '\''; }
+void pr(long double x)  { cout << x; }
+void pr(long long x)  { cout << x; }
+void pr(double x)  { cout << x; }
+void pr(float x)  { cout << x; }
+void pr(long x)  { cout << x; }
+void pr(bool x)  { cout << x; }
+void pr(int x)  { cout << x; }
+
+template <class T, class V> void pr(const pair<T, V> &x);
+template <class T, class V> void pr(const map<T, V>& mp);
+template <class T, class... V> void pr(T t, V... v);
+template <class T> void pr(const multiset<T>& pq);
+template <class T> void pr(const vector<T>& v);
+template <class T> void pr(const set<T>& pq);
+
+template <class T, class V> void pr(const pair<T, V>& x) {
+  cout << "{"; pr(x.first); cout << ", "; pr(x.second); cout << "}";
+}
+template <class T, class V> void pr(const map<T, V>& mp) {
+  for(auto it = mp.begin(); it != mp.end(); ++it) {
+    pr(*it); auto copy = it;
+    if(++copy != mp.end()) 
+      cout << " ";
+  }
+}
+template <class T> void pr(const multiset<T>& pq) {
+  vector<T> values;
+  for(auto& value : pq) 
+    values.push_back(value);
+
+  pr(values);
+}
+template <class T> void pr(const vector<T>& v) {
+  for(int i = 0; i < (int) v.size(); ++i) {
+    pr(v[i]); cout << (i + 1 < (int) v.size() ? " " : "");
+  }
+}
+template <class T> void pr(const set<T>& pq) {
+  vector<T> values;
+  for(auto value : pq)
+    values.push_back(value);
+
+  pr(values);
+}
+template <class T, class... V> void pr(T t, V... v) {
+  pr(t); if(sizeof...(v)) cout << " | ";
+  pr(v...);
+}
+//Global space begins
+
+string last(string s) {
+  int last_space = -1, n = s.size();
+
+  for(int i = n - 1; i >= 0; --i) 
+    if(s[i] == ' ') {
+      last_space = i;
+      break;
+    }
+
+  s = s.substr(last_space + 1);
+  transform(s.begin(), s.end(), s.begin(), ::tolower);
+
+  return s;
+}
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  
+  int z = 1;
+  while(true) {
+    int N;
+    cin >> N;
+    string S;
+
+    if(N == 0)
+      return 0;
+
+    getline(cin, S);
+    map<string, int> freq;
+    for(int i = 0; i < N; ++i) {
+      getline(cin, S);
+      freq[last(S)]++;
+    }
+
+    cout << "List " << z << ":" << '\n';
+    for(auto [name, ff] : freq)
+      cout << name << " | " << ff << '\n';
+
+    z++;
+  }
+}
