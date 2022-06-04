cb9b676fdc7815869f4747dd9f1b9036dc4c99eb
Itish Agarwal <56088263+itish-agarwal@users.noreply.github.com>
2022-06-04 15:36:19 +0530

    Committing files

---
+++ CodeChef/C++14/ZCO15003/51075712.cpp
@@ -0,0 +1,104 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using namespace chrono;
+
+#ifndef ONLINE_JUDGE
+#define debug(x...) cout << "[" << #x << ": "; pr(x); cout << "]\n";
+#else
+#define debug(x...)
+#endif
+
+void pr(string x)  { cout << '\"' << x << '\"'; }
+void pr(char x)  { cout << '\'' << x << '\''; }
+void pr(long double x)  { cout << x; }
+void pr(long long x)  { cout << x; }
+void pr(double x)  { cout << x; }
+void pr(float x)  { cout << x; }
+void pr(long x)  { cout << x; }
+void pr(bool x)  { cout << x; }
+void pr(int x)  { cout << x; }
+
+template <class T, class V> void pr(const pair<T, V> &x);
+template <class T, class V> void pr(const map<T, V>& mp);
+template <class T, class... V> void pr(T t, V... v);
+template <class T> void pr(const multiset<T>& pq);
+template <class T> void pr(const vector<T>& v);
+template <class T> void pr(const set<T>& pq);
+
+template <class T, class V> void pr(const pair<T, V>& x) {
+  cout << "{"; pr(x.first); cout << ", "; pr(x.second); cout << "}";
+}
+template <class T, class V> void pr(const map<T, V>& mp) {
+  for(auto it = mp.begin(); it != mp.end(); ++it) {
+    pr(*it); auto copy = it;
+    if(++copy != mp.end()) 
+      cout << " ";
+  }
+}
+template <class T> void pr(const multiset<T>& pq) {
+  vector<T> values;
+  for(auto& value : pq) 
+    values.push_back(value);
+
+  pr(values);
+}
+template <class T> void pr(const vector<T>& v) {
+  for(int i = 0; i < (int) v.size(); ++i) {
+    pr(v[i]); cout << (i + 1 < (int) v.size() ? " " : "");
+  }
+}
+template <class T> void pr(const set<T>& pq) {
+  vector<T> values;
+  for(auto value : pq)
+    values.push_back(value);
+
+  pr(values);
+}
+template <class T, class... V> void pr(T t, V... v) {
+  pr(t); if(sizeof...(v)) cout << " | ";
+  pr(v...);
+}
+//Global space begins
+
+pair<int, int> intersection(const pair<int, int> &a, const pair<int, int> &b) {
+  return {max(a.first, b.first), min(a.second, b.second)};
+}
+
+void test_case() {
+  int N;
+  cin >> N;
+
+  vector<pair<int, int>> intervals;
+  for(int i = 0; i < N; ++i) {
+    int l, r;
+    cin >> l >> r;
+    intervals.emplace_back(l, r);
+  }
+
+  sort(intervals.begin(), intervals.end());
+
+  int ans = 0, i = 0;
+  while(i < N) {
+    int j = i;
+    pair<int, int> ret = intervals[i];
+    while(j+1 < N && intersection(ret, intervals[j+1]).second >= intersection(ret, intervals[j+1]).first) {
+      ret = intersection(ret, intervals[++j]);
+    }
+    i = j + 1;
+    ans++;  
+  }
+
+  cout << ans << '\n';
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  int tests = 1;
+  // cin >> tests;
+  while(tests--)
+    test_case();
+
+}
