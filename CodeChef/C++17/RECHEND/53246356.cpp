1ebdd4b380a9130aa4b25b3f172dce596f09fef8
Itish Agarwal <56088263+itish-agarwal@users.noreply.github.com>
2022-06-04 15:38:13 +0530

    Committing files

---
+++ CodeChef/C++17/RECHEND/53246356.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using namespace chrono;
+
+#ifndef ONLINE_JUDGE
+#define debug(x...) cout << "[" << #x << ": "; pr(x); cout << "]\n";
+#else
+#define debug(x...)
+#endif
+
+void pr(string x)  { cout << '\"' << x << '\"'; }
+void pr(char x)  { cout << '\'' << x << '\''; }
+void pr(long long x)  { cout << x; }
+void pr(double x)  { cout << x; }
+void pr(bool x)  { cout << x; }
+void pr(int x)  { cout << x; }
+
+template <class T, class V> void pr(const pair<T, V> &x);
+template <class T, class V> void pr(const map<T, V>& mp);
+template <class T, class... V> void pr(T t, V... v);
+template <class T> void pr(const multiset<T>& pq);
+template <class T> void pr(const vector<T>& v);
+template <class T> void pr(const set<T>& pq);
+
+template <class T, class V> void pr(const pair<T, V>& x) {
+  cout << "{"; pr(x.first); cout << ", "; pr(x.second); cout << "}";
+}
+template <class T, class V> void pr(const map<T, V>& mp) {
+  for(auto it = mp.begin(); it != mp.end(); ++it) { pr(*it); cout << " "; }
+}
+template <class T> void pr(const multiset<T>& pq) {
+  vector<T> values(pq.begin(), pq.end()); pr(values);
+}
+template <class T> void pr(const vector<T>& v) {
+  for(int i = 0; i < (int) v.size(); ++i) { pr(v[i]); cout << (i + 1 < (int) v.size() ? " " : ""); }
+}
+template <class T> void pr(const set<T>& pq) {
+  vector<T> values(pq.begin(), pq.end()); pr(values);
+}
+template <class T, class... V> void pr(T t, V... v) {
+  pr(t); if(sizeof...(v)) cout << " | "; pr(v...);
+}
+//Global space begins
+
+void test_case() {
+  int N;
+  cin >> N;
+  vector<int> row(N);
+
+  for(int i = 0; i < N; ++i) {
+    int x, y;
+    cin >> x >> y;
+    --x, --y;
+    row[y] = x;
+  }
+
+  int x = row[N - 1], y = N - 1;
+
+  bool f1 = true, f2 = true;
+
+  for(int i = N - 1; i >= row[N - 1]; --i, x++) 
+    if(row[i] != x) {
+      f1 = false;
+      break;
+    }
+
+  x = row[0];
+  for(int i = 0; i <= row[0]; ++i, x--)
+    if(row[i] != x) {
+      f2 = false;
+      break;
+    }
+
+  debug(f1, f2);
+
+  cout << (f1 || f2 ? "NO" : "YES") << '\n'; 
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  int tests = 1;
+  cin >> tests;
+
+  while(tests--) 
+    test_case();
+}
