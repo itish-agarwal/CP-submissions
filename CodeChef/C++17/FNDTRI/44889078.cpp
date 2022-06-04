1749b938298451d3a539605c82a3dc8e2e8fe55c
Itish Agarwal <56088263+itish-agarwal@users.noreply.github.com>
2022-06-04 15:30:11 +0530

    Committing files

---
+++ CodeChef/C++17/FNDTRI/44889078.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int tt;
+  cin >> tt;
+  while(tt--) {
+    long long a, b;
+    cin >> a >> b;
+    cout << a*b*(b-1)/2 + b*a*(a-1)/2 << '\n';
+  }
+}
