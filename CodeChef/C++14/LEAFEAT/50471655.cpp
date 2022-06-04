4450aad18c98dffc88da8db3b459828532a6ec69
Itish Agarwal <56088263+itish-agarwal@users.noreply.github.com>
2022-06-04 15:26:34 +0530

    Committing files

---
+++ CodeChef/C++14/LEAFEAT/50471655.cpp
@@ -0,0 +1,107 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using namespace chrono;
+
+#ifndef ONLINE_JUDGE
+#define debug(x...) cout << "[" << #x << ": "; pr(x); cout << "]\n";
+#else
+#define debug(x...)
+#endif
+
+void pr(string x)  { cout << '\"' << x << '\"'; }
+void pr(char x)  { cout << '\'' << x << '\''; }
+void pr(long double x)  { cout << x; }
+void pr(long long x)  { cout << x; }
+void pr(double x)  { cout << x; }
+void pr(float x)  { cout << x; }
+void pr(long x)  { cout << x; }
+void pr(bool x)  { cout << x; }
+void pr(int x)  { cout << x; }
+
+template <class T, class V> void pr(const pair<T, V> &x);
+template <class T, class V> void pr(const map<T, V>& mp);
+template <class T, class... V> void pr(T t, V... v);
+template <class T> void pr(const multiset<T>& pq);
+template <class T> void pr(const vector<T>& v);
+template <class T> void pr(const set<T>& pq);
+
+template <class T, class V> void pr(const pair<T, V>& x) {
+  cout << "{"; pr(x.first); cout << ", "; pr(x.second); cout << "}";
+}
+template <class T, class V> void pr(const map<T, V>& mp) {
+  for(auto it = mp.begin(); it != mp.end(); ++it) {
+    pr(*it); auto copy = it;
+    if(++copy != mp.end()) 
+      cout << " ";
+  }
+}
+template <class T> void pr(const multiset<T>& pq) {
+  vector<T> values;
+  for(auto& value : pq) 
+    values.push_back(value);
+
+  pr(values);
+}
+template <class T> void pr(const vector<T>& v) {
+  for(int i = 0; i < (int) v.size(); ++i) {
+    pr(v[i]); cout << (i + 1 < (int) v.size() ? " " : "");
+  }
+}
+template <class T> void pr(const set<T>& pq) {
+  vector<T> values;
+  for(auto value : pq)
+    values.push_back(value);
+
+  pr(values);
+}
+template <class T, class... V> void pr(T t, V... v) {
+  pr(t); if(sizeof...(v)) cout << " | ";
+  pr(v...);
+}
+//Global space begins
+
+void test_case() {
+  int N, K;
+  cin >> N >> K;
+
+  vector<int> A(K);
+  for(auto& a : A)
+    cin >> a;
+
+  int ans = N;
+
+  for (int mask = 1; mask < 1 << K; ++mask) {
+    int k = __builtin_popcount(mask);
+    long long lcm = 1, ok = 1;
+    for (int i = 0; i < K; ++i) {
+      if (mask & (1 << i)) {
+        lcm = (lcm * A[i]) / __gcd(lcm, (long long) A[i]);
+        if(lcm + 1 > N) {
+          ok = 0;
+          break;
+        }
+      }
+    }
+
+    if(ok) {
+      ans += (k % 2 ? -1 : 1) * (N - 1 + lcm) / lcm;
+    }
+    else {
+      ans += (k % 2 ? -1 : 1);
+    }
+  }
+
+  cout << ans << '\n';
+} 
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  int tests = 1;
+  // cin >> tests;
+  while(tests--)
+    test_case();
+
+}
