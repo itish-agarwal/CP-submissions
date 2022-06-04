eacf8a8f62cee992dec2016ac4115833002e30c9
Itish Agarwal <56088263+itish-agarwal@users.noreply.github.com>
2022-06-04 15:32:39 +0530

    Committing files

---
+++ CodeChef/C++17/CHFNSW/41928138.cpp
@@ -0,0 +1,21 @@
+#ifndef ONLINE_JUDGE
+  #include "pch.h"
+#else 
+  #include <bits/stdc++.h>
+#endif
+
+using namespace std;
+#define int long long
+
+signed main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int tt;
+  cin >> tt;
+  while(tt--) {
+    int n, m;
+    cin >> n >> m;
+    cout << n*m / 2 << '\n';
+  }
+  return 0;
+}
