efc9db5a358dc22d987cb56a070e5a6be86415b7
Itish Agarwal <56088263+itish-agarwal@users.noreply.github.com>
2022-06-04 15:32:28 +0530

    Committing files

---
+++ CodeChef/C++17/ROCPERM/41953854.cpp
@@ -0,0 +1,25 @@
+#ifndef ONLINE_JUDGE
+  #include "pch.h"
+#else 
+  #include <bits/stdc++.h>
+#endif
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int tt;
+  cin >> tt;
+  while(tt--) {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for(int i = 0; i < n; i++) {
+      cin >> a[i];
+      cout << i+1 << " ";
+    }
+    cout << '\n';
+  }
+  return 0;
+}
