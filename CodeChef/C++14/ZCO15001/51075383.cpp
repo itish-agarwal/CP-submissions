21c9f2a40c79f52296742ab9bd32b6e54e857bfb
Itish Agarwal <56088263+itish-agarwal@users.noreply.github.com>
2022-06-04 15:36:20 +0530

    Committing files

---
+++ CodeChef/C++14/ZCO15001/51075383.cpp
@@ -0,0 +1,108 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using namespace chrono;
+
+#ifndef ONLINE_JUDGE
+#define debug(x...) cout << "[" << #x << ": "; pr(x); cout << "]\n";
+#else
+#define debug(x...)
+#endif
+
+void pr(string x)  { cout << '\"' << x << '\"'; }
+void pr(char x)  { cout << '\'' << x << '\''; }
+void pr(long double x)  { cout << x; }
+void pr(long long x)  { cout << x; }
+void pr(double x)  { cout << x; }
+void pr(float x)  { cout << x; }
+void pr(long x)  { cout << x; }
+void pr(bool x)  { cout << x; }
+void pr(int x)  { cout << x; }
+
+template <class T, class V> void pr(const pair<T, V> &x);
+template <class T, class V> void pr(const map<T, V>& mp);
+template <class T, class... V> void pr(T t, V... v);
+template <class T> void pr(const multiset<T>& pq);
+template <class T> void pr(const vector<T>& v);
+template <class T> void pr(const set<T>& pq);
+
+template <class T, class V> void pr(const pair<T, V>& x) {
+  cout << "{"; pr(x.first); cout << ", "; pr(x.second); cout << "}";
+}
+template <class T, class V> void pr(const map<T, V>& mp) {
+  for(auto it = mp.begin(); it != mp.end(); ++it) {
+    pr(*it); auto copy = it;
+    if(++copy != mp.end()) 
+      cout << " ";
+  }
+}
+template <class T> void pr(const multiset<T>& pq) {
+  vector<T> values;
+  for(auto& value : pq) 
+    values.push_back(value);
+
+  pr(values);
+}
+template <class T> void pr(const vector<T>& v) {
+  for(int i = 0; i < (int) v.size(); ++i) {
+    pr(v[i]); cout << (i + 1 < (int) v.size() ? " " : "");
+  }
+}
+template <class T> void pr(const set<T>& pq) {
+  vector<T> values;
+  for(auto value : pq)
+    values.push_back(value);
+
+  pr(values);
+}
+template <class T, class... V> void pr(T t, V... v) {
+  pr(t); if(sizeof...(v)) cout << " | ";
+  pr(v...);
+}
+//Global space begins
+
+void test_case() {
+  int N;
+  cin >> N;
+
+  vector<int> A(N);
+  for(auto &a : A)
+    cin >> a;
+
+  vector<vector<int>> is_palin(N, vector<int> (N));
+
+  for(int L = N - 1; L >= 0; --L) {
+    for(int R = L; R < N; ++R) {
+      if(L == R) {
+        is_palin[L][R] = 1;
+      }
+      else {
+        is_palin[L][R] = (A[L] == A[R] && (L + 1 <= R - 1 ? is_palin[L + 1][R - 1] : 1));
+      }
+    }
+  }
+
+  vector<int> dp(N, N);
+  dp[N - 1] = 1;
+
+  for(int i = N - 2; i >= 0; --i) {
+    for(int j = i; j < N; ++j) {
+      if(is_palin[i][j]) {
+        dp[i] = min(dp[i], 1 + (j == N - 1 ? 0 : dp[j + 1]));
+      }
+    }
+  }
+
+  cout << dp[0] << '\n';
+} 
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  int tests = 1;
+  // cin >> tests;
+  while(tests--)
+    test_case();
+
+}
