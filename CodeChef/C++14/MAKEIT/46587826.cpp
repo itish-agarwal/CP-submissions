f0e8124e7cc2559c4e7f1fb31d61359e2cc1a57c
Itish Agarwal <56088263+itish-agarwal@users.noreply.github.com>
2022-06-04 15:29:07 +0530

    Committing files

---
+++ CodeChef/C++14/MAKEIT/46587826.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int tt;
+  cin >> tt;
+  while(tt--) {
+    int a, b;
+    cin >> a >> b;
+    if(a != 0 && b != 0) {
+      cout << "YES\n";
+    } else {
+      cout << "NO\n";
+    }
+  }
+}
