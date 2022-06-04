5edc1578c15cb8e64cdbb56adc3e3e2d6676d6e7
Itish Agarwal <56088263+itish-agarwal@users.noreply.github.com>
2022-06-04 15:19:38 +0530

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc215_a/25252497.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  string S;
+  cin >> S;
+
+  cout << (S == "Hello,World!" ? "AC" : "WA") << '\n';
+}
