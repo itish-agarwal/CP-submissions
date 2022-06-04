00d8f943d2fdfbc622b771c54f23b58bf33c1d84
Itish Agarwal <56088263+itish-agarwal@users.noreply.github.com>
2022-06-04 15:22:43 +0530

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc184_a/18303424.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define int long long
+
+signed main(){
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int a, b, c, d;
+  cin >> a >> b >> c >> d;
+  cout << a*d - b*c << endl;
+  return 0;
+}
