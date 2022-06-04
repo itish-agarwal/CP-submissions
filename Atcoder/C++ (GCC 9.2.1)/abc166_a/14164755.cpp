e594a1ba64c01e8c8671d11c5469a3c5b98dd8a1
Itish Agarwal <56088263+itish-agarwal@users.noreply.github.com>
2022-06-04 15:24:32 +0530

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc166_a/14164755.cpp
@@ -0,0 +1,22 @@
+#include "bits/stdc++.h"
+#pragma optimize("unroll-loops");
+#pragma optimize("Ofast");
+//#include "pch.h"    
+using namespace std;
+unordered_map<int, bool> here;
+ 
+using ll = long long;
+const int nax = 5e5 + 2;
+const ll mod = 1e9 + 7;
+
+int main() {
+    
+    string s;
+    cin >> s;
+    if(s == "ABC") {
+        cout << "ARC\n";
+    }else{
+        cout << "ABC\n";
+    }
+    return 0;
+}
