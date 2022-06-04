bd004422a0c9ce5bded223339db64885f975bade
Itish Agarwal <56088263+itish-agarwal@users.noreply.github.com>
2022-06-04 15:42:02 +0530

    Committing files

---
+++ CodeChef/C++17/ITGUY18/40659107.cpp
@@ -0,0 +1,33 @@
+
+#include <bits/stdc++.h>
+
+using namespace std;
+#define int int64_t 
+
+ostream& operator << (ostream& stream, const vector<int>& v) {
+  for(int x : v) {
+    stream << x << " ";
+  }
+  return stream;
+}
+
+//ostream& operator << (ostream& stream, const int *a) {
+  //for(int i = 0; i < n; i++) {
+    //stream << *(a + i) <<  " ";
+  //}
+  //return stream;
+//}
+
+signed main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int tt;
+  cin >> tt;
+  while(tt--) {
+    int n;
+    cin >> n;
+    int ans = n*(n+1)*(2*n+1);
+    cout << ans/6 << '\n';
+  }
+  return 0;
+}
