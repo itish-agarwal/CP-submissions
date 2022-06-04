879ce20e23bf37e3a1f504cb8ec33fe085120c7f
Itish Agarwal <56088263+itish-agarwal@users.noreply.github.com>
2022-06-04 15:45:59 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1680A | Minimums and Maximums/157018077.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+#ifndef ONLINE_JUDGE
+#define debug(x...) cout << "[" << #x << ": "; pr(x); cout << "]\n";
+#else
+#define debug(x...)
+#endif
+ 
+void pr(string x)  { cout << '\"' << x << '\"'; }
+void pr(long long x)  { cout << x; }
+void pr(bool x)  { cout << x; }
+void pr(int x)  { cout << x; }
+ 
+template <class T, class V> void pr(const map<T, V>& mp);
+template <class T, class... V> void pr(T t, V... v);
+template <class T> void pr(const multiset<T>& pq);
+template <class T> void pr(const vector<T>& v);
+ 
+template <class T, class V> void pr(const map<T, V>& mp) {
+   for (auto it = mp.begin(); it != mp.end(); ++it) { pr(*it); cout << " "; }
+}
+template <class T> void pr(const multiset<T>& pq) {
+   vector<T> values(pq.begin(), pq.end()); pr(values);
+}
+template <class T> void pr(const vector<T>& v) {
+   for (int i = 0; i < (int) v.size(); ++i) { pr(v[i]); cout << (i + 1 < (int) v.size() ? " " : ""); }
+}
+template <class T, class... V> void pr(T t, V... v) {
+   pr(t); if(sizeof...(v)) cout << " | "; pr(v...);
+}
+//Global space begins
+ 
+void test_case() {
+   int l1, r1, l2, r2;
+   cin >> l1 >> r1 >> l2 >> r2;
+ 
+   if (l1 > l2) {
+      swap (l1, l2);
+      swap (r1, r2);
+   }
+ 
+   if (r1 >= l2) {
+      cout << max (l1, l2) << '\n';
+   }
+   else {
+      cout << l1 + l2 << '\n';
+   }
+}
+ 
+int main() {
+   ios::sync_with_stdio(false);
+   cin.tie(0);
+ 
+   int tt = 1;
+   cin >> tt;
+ 
+   while(tt--)
+      test_case();
+}
