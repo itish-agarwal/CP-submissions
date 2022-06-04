f40465c8f55d2a73397c022412ba326b8b16fea8
Itish Agarwal <56088263+itish-agarwal@users.noreply.github.com>
2022-06-04 15:41:53 +0530

    Committing files

---
+++ CodeChef/C++17/NUMGAME/40707093.cpp
@@ -0,0 +1,38 @@
+
+#include <bits/stdc++.h>
+
+using namespace std;
+#define int int64_t 
+
+ostream& operator << (ostream& stream, const vector<int>& v) {
+  for(int x : v) {
+    stream << x << " ";
+  }
+  return stream;
+}
+
+//ostream& operator << (ostream& stream, const int *a) {
+  //for(int i = 0; i < n; i++) {
+    //stream << *(a + i) <<  " ";
+  //}
+  //return stream;
+//}
+
+//ABCDEFGHIJKLMNOPQRSTUVWXYZ
+
+signed main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int tt;
+  cin >> tt;
+  while(tt--) {
+    int n;
+    cin >> n;
+    if(n % 2) {
+      cout << "BOB\n";
+    } else {
+      cout << "ALICE\n";
+    }
+  }
+  return 0;
+}
