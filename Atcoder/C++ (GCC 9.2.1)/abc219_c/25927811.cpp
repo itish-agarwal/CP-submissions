766819dcc21cca22479fb656020c9c6a3d7df0af
Itish Agarwal <56088263+itish-agarwal@users.noreply.github.com>
2022-06-04 15:18:58 +0530

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc219_c/25927811.cpp
@@ -0,0 +1,110 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using namespace chrono;
+
+#ifndef ONLINE_JUDGE
+#define debug(x...) cout << "[" << #x << ": "; pr(x); cout << "]\n";
+#else
+#define debug(x...)
+#endif
+
+void pr(string x)  { cout << '\"' << x << '\"'; }
+void pr(char x)  { cout << '\'' << x << '\''; }
+void pr(long double x)  { cout << x; }
+void pr(long long x)  { cout << x; }
+void pr(double x)  { cout << x; }
+void pr(float x)  { cout << x; }
+void pr(long x)  { cout << x; }
+void pr(bool x)  { cout << x; }
+void pr(int x)  { cout << x; }
+
+template <class T, class V> void pr(const pair<T, V> &x);
+template <class T, class V> void pr(const map<T, V>& mp);
+template <class T, class... V> void pr(T t, V... v);
+template <class T> void pr(const multiset<T>& pq);
+template <class T> void pr(const vector<T>& v);
+template <class T> void pr(const set<T>& pq);
+
+template <class T, class V> void pr(const pair<T, V>& x) {
+  cout << "{"; pr(x.first); cout << ", "; pr(x.second); cout << "}";
+}
+template <class T, class V> void pr(const map<T, V>& mp) {
+  for(auto it = mp.begin(); it != mp.end(); ++it) {
+    pr(*it); auto copy = it;
+    if(++copy != mp.end()) 
+      cout << " ";
+  }
+}
+template <class T> void pr(const multiset<T>& pq) {
+  vector<T> values;
+  for(auto& value : pq) 
+    values.push_back(value);
+
+  pr(values);
+}
+template <class T> void pr(const vector<T>& v) {
+  for(int i = 0; i < (int) v.size(); ++i) {
+    pr(v[i]); cout << (i + 1 < (int) v.size() ? " " : "");
+  }
+}
+template <class T> void pr(const set<T>& pq) {
+  vector<T> values;
+  for(auto value : pq)
+    values.push_back(value);
+
+  pr(values);
+}
+template <class T, class... V> void pr(T t, V... v) {
+  pr(t); if(sizeof...(v)) cout << " | ";
+  pr(v...);
+}
+//Global space begins
+
+int location[26];
+string order;
+
+bool compare(const string& a, const string& b) {
+  int n = a.size(), m = b.size();
+  for(int i = 0; i < min(n, m); ++i) {
+    if(a[i] != b[i]) {
+      if(location[a[i]-'a'] < location[b[i]-'a']) {
+        return true;
+      }
+      return false;
+    }
+  }
+
+  if(n > m) 
+    return false;
+
+  return true;
+}
+
+void test_case() {
+  cin >> order;
+  for(int i = 0; i < 26; ++i)
+    location[order[i]-'a'] = i;
+
+  int N;
+  cin >> N;
+
+  vector<string> S(N);
+  for(auto &s : S)
+    cin >> s;
+
+  sort(S.begin(), S.end(), compare);
+  for(auto &s : S)
+    cout << s << '\n';
+}   
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  int tests = 1;
+  // cin >> tests;
+  while(tests--)
+    test_case();
+
+}
