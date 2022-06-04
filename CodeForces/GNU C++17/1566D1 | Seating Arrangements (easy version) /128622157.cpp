6d70f08caaa81a4d9f9559fe1f168f8dae8e9b59
Itish Agarwal <56088263+itish-agarwal@users.noreply.github.com>
2022-06-04 15:52:48 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1566D1 | Seating Arrangements (easy version) /128622157.cpp
@@ -0,0 +1,105 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+using namespace chrono;
+ 
+#ifndef ONLINE_JUDGE
+#define debug(x...) cout << "[" << #x << ": "; pr(x); cout << "]\n";
+#else
+#define debug(x...)
+#endif
+ 
+void pr(string x)  { cout << '\"' << x << '\"'; }
+void pr(char x)  { cout << '\'' << x << '\''; }
+void pr(long double x)  { cout << x; }
+void pr(long long x)  { cout << x; }
+void pr(double x)  { cout << x; }
+void pr(float x)  { cout << x; }
+void pr(long x)  { cout << x; }
+void pr(bool x)  { cout << x; }
+void pr(int x)  { cout << x; }
+ 
+template <class T, class V> void pr(const pair<T, V> &x);
+template <class T, class V> void pr(const map<T, V>& mp);
+template <class T, class... V> void pr(T t, V... v);
+template <class T> void pr(const multiset<T>& pq);
+template <class T> void pr(const vector<T>& v);
+template <class T> void pr(const set<T>& pq);
+ 
+template <class T, class V> void pr(const pair<T, V>& x) {
+  cout << "{"; pr(x.first); cout << ", "; pr(x.second); cout << "}";
+}
+template <class T, class V> void pr(const map<T, V>& mp) {
+  for(auto it = mp.begin(); it != mp.end(); ++it) {
+    pr(*it); auto copy = it;
+    if(++copy != mp.end()) 
+      cout << " ";
+  }
+}
+template <class T> void pr(const multiset<T>& pq) {
+  vector<T> values;
+  for(auto& value : pq) 
+    values.push_back(value);
+ 
+  pr(values);
+}
+template <class T> void pr(const vector<T>& v) {
+  for(int i = 0; i < (int) v.size(); ++i) {
+    pr(v[i]); cout << (i + 1 < (int) v.size() ? " " : "");
+  }
+}
+template <class T> void pr(const set<T>& pq) {
+  vector<T> values;
+  for(auto value : pq)
+    values.push_back(value);
+ 
+  pr(values);
+}
+template <class T, class... V> void pr(T t, V... v) {
+  pr(t); if(sizeof...(v)) cout << " | ";
+  pr(v...);
+}
+//Global space begins
+ 
+void test_case() {
+  int N, M;
+  cin >> N >> M;
+ 
+  vector<int> people(M);
+  for(auto &p : people) {
+    cin >> p;
+    p -= 1;
+  }
+ 
+  vector<int> x = people;
+  sort(x.begin(), x.end());
+ 
+  map<int, int> index;
+  for(int i = 0; i < M; ++i)
+    index[x[i]] = i;
+ 
+  vector<int> seats(M);
+  long long ans = 0;
+ 
+  for(int i = 0; i < M; ++i) {
+    int id = index[people[i]];
+    for(int j = 0; j < id; ++j) {
+      ans += seats[j];
+    }
+    seats[id] += 1;
+    index[people[i]] -= 1;
+  }
+ 
+  cout << ans << '\n';
+} 
+ 
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+ 
+  int tests = 1;
+  cin >> tests;
+  while(tests--)
+    test_case();
+ 
+}
