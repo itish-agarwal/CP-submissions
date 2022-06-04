c4800d0f27eb3cf6d9f5284464d63a08cf3c0219
Itish Agarwal <56088263+itish-agarwal@users.noreply.github.com>
2022-06-04 15:53:21 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1567B | MEXor Mixup/127931247.cpp
@@ -0,0 +1,100 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+using namespace chrono;
+ 
+#ifndef ONLINE_JUDGE
+#define debug(x...) cout << "[" << #x << ": "; pr(x); cout << "]\n";
+#else
+#define debug(x...)
+#endif
+ 
+void pr(string x)  { cout << '\"' << x << '\"'; }
+void pr(char x)  { cout << '\'' << x << '\''; }
+void pr(long double x)  { cout << x; }
+void pr(long long x)  { cout << x; }
+void pr(double x)  { cout << x; }
+void pr(float x)  { cout << x; }
+void pr(long x)  { cout << x; }
+void pr(bool x)  { cout << x; }
+void pr(int x)  { cout << x; }
+ 
+template <class T, class V> void pr(const pair<T, V> &x);
+template <class T, class V> void pr(const map<T, V>& mp);
+template <class T, class... V> void pr(T t, V... v);
+template <class T> void pr(const multiset<T>& pq);
+template <class T> void pr(const vector<T>& v);
+template <class T> void pr(const set<T>& pq);
+ 
+template <class T, class V> void pr(const pair<T, V>& x) {
+  cout << "{"; pr(x.first); cout << ", "; pr(x.second); cout << "}";
+}
+template <class T, class V> void pr(const map<T, V>& mp) {
+  for(auto it = mp.begin(); it != mp.end(); ++it) {
+    pr(*it); auto copy = it;
+    if(++copy != mp.end()) 
+      cout << " ";
+  }
+}
+template <class T> void pr(const multiset<T>& pq) {
+  vector<T> values;
+  for(auto& value : pq) 
+    values.push_back(value);
+ 
+  pr(values);
+}
+template <class T> void pr(const vector<T>& v) {
+  for(int i = 0; i < (int) v.size(); ++i) {
+    pr(v[i]); cout << (i + 1 < (int) v.size() ? " " : "");
+  }
+}
+template <class T> void pr(const set<T>& pq) {
+  vector<T> values;
+  for(auto value : pq)
+    values.push_back(value);
+ 
+  pr(values);
+}
+template <class T, class... V> void pr(T t, V... v) {
+  pr(t); if(sizeof...(v)) cout << " | ";
+  pr(v...);
+}
+//Global space begins
+ 
+int upto(int N) {
+  if(N % 4 == 0)
+    return N;
+ 
+  if(N % 4 == 1) 
+    return 1;
+ 
+  if(N % 4 == 2)
+    return N + 1;
+ 
+  return 0;
+}
+  
+void test_case() {
+  int A, B;
+  cin >> A >> B;
+ 
+  int X = upto(A - 1);
+ 
+  if(X == B) {
+    cout << A << '\n';
+    return;
+  }
+ 
+  cout << ((X ^ B) == A ? A + 2 : A + 1) << '\n';
+} 
+ 
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+ 
+  int tests;
+  cin >> tests;
+  while(tests--)
+    test_case();
+ 
+}
