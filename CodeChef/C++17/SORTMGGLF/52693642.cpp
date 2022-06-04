75e6fec27ddf3b12d9228b233593b65346e8a094
Itish Agarwal <56088263+itish-agarwal@users.noreply.github.com>
2022-06-04 15:26:14 +0530

    Committing files

---
+++ CodeChef/C++17/SORTMGGLF/52693642.cpp
@@ -0,0 +1,2 @@
+#include <iostream>
+using namespace std;int main() {int n;cin>>n;int a[n],b[n];for(int i=0;i<n;++i){cin>>a[i];}for(int i=0;i<n;++i){cin>>b[i];}int c[n];for(int i=0;i<n;++i){c[--b[i]]=a[i];}for(int i=0;i<n;++i){cout<<c[i]<<" ";}}
