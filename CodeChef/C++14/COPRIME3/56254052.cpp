2ae980229cd8953fb82c19dbea5ceb46b74ce79a
Itish Agarwal <56088263+itish-agarwal@users.noreply.github.com>
2022-06-04 15:37:23 +0530

    Committing files

---
+++ CodeChef/C++14/COPRIME3/56254052.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+using namespace chrono;
+ 
+#ifndef ONLINE_JUDGE
+#define debug(x...) cout << "[" << #x << ": "; pr(x); cout << "]\n";
+#else
+#define debug(x...)
+#endif
+ 
+void pr(string x)  { cout << '\"' << x << '\"'; }
+void pr(char x)  { cout << '\'' << x << '\''; }
+void pr(long long x)  { cout << x; }
+void pr(double x)  { cout << x; }
+void pr(bool x)  { cout << x; }
+void pr(int x)  { cout << x; }
+ 
+template <class T, class V> void pr(const pair<T, V> &x);
+template <class T, class V> void pr(const map<T, V>& mp);
+template <class T, class... V> void pr(T t, V... v);
+template <class T> void pr(const multiset<T>& pq);
+template <class T> void pr(const vector<T>& v);
+template <class T> void pr(const set<T>& pq);
+ 
+template <class T, class V> void pr(const pair<T, V>& x) {
+   cout << "{"; pr(x.first); cout << ", "; pr(x.second); cout << "}";
+}
+template <class T, class V> void pr(const map<T, V>& mp) {
+   for(auto it = mp.begin(); it != mp.end(); ++it) { pr(*it); cout << " "; }
+}
+template <class T> void pr(const multiset<T>& pq) {
+   vector<T> values(pq.begin(), pq.end()); pr(values);
+}
+template <class T> void pr(const vector<T>& v) {
+   for(int i = 0; i < (int) v.size(); ++i) { pr(v[i]); cout << (i + 1 < (int) v.size() ? " " : ""); }
+}
+template <class T> void pr(const set<T>& pq) {
+   vector<T> values(pq.begin(), pq.end()); pr(values);
+}
+template <class T, class... V> void pr(T t, V... v) {
+   pr(t); if(sizeof...(v)) cout << " | "; pr(v...);
+}
+//Global space begins
+using lli = long long;
+const int nax = 1e6 + 7;
+lli cnt[nax];
+ 
+void test_case() {
+   int N;
+   cin >> N;
+   for (int i = 0; i < N; ++i) {
+      int a;
+      cin >> a;
+      cnt[a]++;
+   }
+ 
+   for (int i = 1; i < nax; ++i)
+      for (int j = 2 * i; j < nax; j += i)
+         cnt[i] += cnt[j];
+ 
+   for (int i = 1; i < nax; ++i)
+      cnt[i] = cnt[i] * (cnt[i] - 1) * (cnt[i] - 2) / 6;
+ 
+   for (int i = nax - 1; i >= 1; --i)
+      for (int j = 2 * i; j < nax; j += i)
+         cnt[i] -= cnt[j];
+ 
+   cout << cnt[1] << '\n';
+}
+ 
+int main() {
+   ios::sync_with_stdio(false);
+   cin.tie(0);
+ 
+   int tests = 1;
+   // cin >> tests;
+ 
+   while(tests--)
+      test_case();
+}
