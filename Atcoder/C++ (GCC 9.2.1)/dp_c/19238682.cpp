a23f1879d36de042d5c4119d38f7297436a21685
Itish Agarwal <56088263+itish-agarwal@users.noreply.github.com>
2022-06-04 15:22:16 +0530

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/dp_c/19238682.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int n;
+  cin >> n;
+  vector<int> a(n), b(n), c(n);
+  for(int i = 0; i < n; i++) {
+    cin >> a[i] >> b[i] >> c[i];
+  }
+  vector<vector<int>> dp(n+1, vector<int> (3, 0));
+  for(int i = n-1; i >= 0; i--) {
+    dp[i][0] = a[i] + max(dp[i+1][1], dp[i+1][2]);
+    dp[i][1] = b[i] + max(dp[i+1][0], dp[i+1][2]);
+    dp[i][2] = c[i] + max(dp[i+1][0], dp[i+1][1]);
+  }
+  cout << max(dp[0][0], max(dp[0][1], dp[0][2])) << '\n';
+  return 0;
+}
