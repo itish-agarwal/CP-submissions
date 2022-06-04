aa59eca2bf283edbba016d5153c7640b1360a9f0
Itish Agarwal <56088263+itish-agarwal@users.noreply.github.com>
2022-06-04 15:51:38 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1594E1 | Rubik's Cube Coloring (easy version)/131186652.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+using namespace chrono;
+ 
+#ifndef ONLINE_JUDGE
+#define debug(x...) cout << "[" << #x << ": "; pr(x); cout << "]\n";
+#else
+#define debug(x...)
+#endif
+#define int long long
+ 
+void pr(string x)  { cout << '\"' << x << '\"'; }
+void pr(char x)  { cout << '\'' << x << '\''; }
+void pr(long double x)  { cout << x; }
+void pr(long long x)  { cout << x; }
+void pr(double x)  { cout << x; }
+void pr(float x)  { cout << x; }
+void pr(long x)  { cout << x; }
+void pr(bool x)  { cout << x; }
+// void pr(int x)  { cout << x; }
+ 
+template <class T, class V> void pr(const pair<T, V> &x);
+template <class T, class V> void pr(const map<T, V>& mp);
+template <class T, class... V> void pr(T t, V... v);
+template <class T> void pr(const multiset<T>& pq);
+template <class T> void pr(const vector<T>& v);
+template <class T> void pr(const set<T>& pq);
+ 
+template <class T, class V> void pr(const pair<T, V>& x) {
+  cout << "{"; pr(x.first); cout << ", "; pr(x.second); cout << "}";
+}
+template <class T, class V> void pr(const map<T, V>& mp) {
+  for(auto it = mp.begin(); it != mp.end(); ++it) { pr(*it); cout << " "; }
+}
+template <class T> void pr(const multiset<T>& pq) {
+  vector<T> values(pq.begin(), pq.end()); pr(values);
+}
+template <class T> void pr(const vector<T>& v) {
+  for(int i = 0; i < (int) v.size(); ++i) { pr(v[i]); cout << (i + 1 < (int) v.size() ? " " : ""); }
+}
+template <class T> void pr(const set<T>& pq) {
+  vector<T> values(pq.begin(), pq.end()); pr(values);
+}
+template <class T, class... V> void pr(T t, V... v) {
+  pr(t); if(sizeof...(v)) cout << " | ";
+  pr(v...);
+}
+//Global space begins
+const int mod = 1e9 + 7;
+ 
+int power(int a, int b, int MOD) {
+  if(b == 0) 
+    return 1;
+ 
+  int res = power(a, b / 2, MOD);
+  res = (res * res) % MOD;
+  if(b % 2) 
+    res = (res * a) % MOD;
+ 
+  return res;
+}
+ 
+void add_self(int& a, int b) {
+  a += b;
+  if(a >= mod) {
+    a -= mod;
+  }
+}
+ 
+void test_case() {
+  int K;
+  cin >> K;
+ 
+  int result = (power(2, K, mod - 1) - 1 + mod - 1) % mod;
+  result = power(4, result, mod);
+  cout << (6 * result) % mod << '\n';
+}
+ 
+signed main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+ 
+  int tests = 1;
+  // cin >> tests;
+ 
+  while(tests--) 
+    test_case();
+}
