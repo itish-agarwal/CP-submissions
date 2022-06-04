a4dd902a0303e43d6f06eac1b4eb0c7a17110244
Itish Agarwal <56088263+itish-agarwal@users.noreply.github.com>
2022-06-04 15:36:59 +0530

    Committing files

---
+++ CodeChef/C++17/BFOC/63109172.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+#define debug(x...) cout << "[" << #x << ": "; pr(x); cout << "]\n";
+#else
+#define debug(x...)
+#endif
+
+void pr(string x)  { cout << '\"' << x << '\"'; }
+void pr(long long x)  { cout << x; }
+void pr(bool x)  { cout << x; }
+void pr(int x)  { cout << x; }
+
+template <class T, class V> void pr(const map<T, V>& mp);
+template <class T, class... V> void pr(T t, V... v);
+template <class T> void pr(const multiset<T>& pq);
+template <class T> void pr(const vector<T>& v);
+
+template <class T, class V> void pr(const map<T, V>& mp) {
+   for (auto it = mp.begin(); it != mp.end(); ++it) { pr(*it); cout << " "; }
+}
+template <class T> void pr(const multiset<T>& pq) {
+   vector<T> values(pq.begin(), pq.end()); pr(values);
+}
+template <class T> void pr(const vector<T>& v) {
+   for (int i = 0; i < (int) v.size(); ++i) { pr(v[i]); cout << (i + 1 < (int) v.size() ? " " : ""); }
+}
+template <class T, class... V> void pr(T t, V... v) {
+   pr(t); if(sizeof...(v)) cout << " | "; pr(v...);
+}
+//Global space begins
+
+void test_case() {
+   int64_t N, M, L;
+   cin >> N >> M >> L;
+
+   int64_t left = M % (L + N - 1);
+
+   if (N > 0)
+      cout << (left < L ? left : 0) << '\n';
+   else
+      cout << M << '\n';
+}
+
+int main() {
+   ios::sync_with_stdio(false);
+   cin.tie(0);
+
+   int tt = 1;
+   cin >> tt;
+
+   while(tt--)
+      test_case();
+}
