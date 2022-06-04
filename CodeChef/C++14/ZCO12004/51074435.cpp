58f39abe5422ad5504d5bf899ade900d13be759a
Itish Agarwal <56088263+itish-agarwal@users.noreply.github.com>
2022-06-04 15:36:21 +0530

    Committing files

---
+++ CodeChef/C++14/ZCO12004/51074435.cpp
@@ -0,0 +1,108 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using namespace chrono;
+
+#ifndef ONLINE_JUDGE
+#define debug(x...) cout << "[" << #x << ": "; pr(x); cout << "]\n";
+#else
+#define debug(x...)
+#endif
+
+void pr(string x)  { cout << '\"' << x << '\"'; }
+void pr(char x)  { cout << '\'' << x << '\''; }
+void pr(long double x)  { cout << x; }
+void pr(long long x)  { cout << x; }
+void pr(double x)  { cout << x; }
+void pr(float x)  { cout << x; }
+void pr(long x)  { cout << x; }
+void pr(bool x)  { cout << x; }
+void pr(int x)  { cout << x; }
+
+template <class T, class V> void pr(const pair<T, V> &x);
+template <class T, class V> void pr(const map<T, V>& mp);
+template <class T, class... V> void pr(T t, V... v);
+template <class T> void pr(const multiset<T>& pq);
+template <class T> void pr(const vector<T>& v);
+template <class T> void pr(const set<T>& pq);
+
+template <class T, class V> void pr(const pair<T, V>& x) {
+  cout << "{"; pr(x.first); cout << ", "; pr(x.second); cout << "}";
+}
+template <class T, class V> void pr(const map<T, V>& mp) {
+  for(auto it = mp.begin(); it != mp.end(); ++it) {
+    pr(*it); auto copy = it;
+    if(++copy != mp.end()) 
+      cout << " ";
+  }
+}
+template <class T> void pr(const multiset<T>& pq) {
+  vector<T> values;
+  for(auto& value : pq) 
+    values.push_back(value);
+
+  pr(values);
+}
+template <class T> void pr(const vector<T>& v) {
+  for(int i = 0; i < (int) v.size(); ++i) {
+    pr(v[i]); cout << (i + 1 < (int) v.size() ? " " : "");
+  }
+}
+template <class T> void pr(const set<T>& pq) {
+  vector<T> values;
+  for(auto value : pq)
+    values.push_back(value);
+
+  pr(values);
+}
+template <class T, class... V> void pr(T t, V... v) {
+  pr(t); if(sizeof...(v)) cout << " | ";
+  pr(v...);
+}
+//Global space begins
+
+const long long inf = 2e15 + 9;
+const int MAX_N = 1e6 + 7;
+int N, A[MAX_N];
+long long dp[MAX_N][5][5];
+
+long long rec(int i, int took_first, int missed) {
+  if(i == N - 1) {
+    if(took_first) {
+      return missed > 0 ? A[i] : 0;
+    }
+    else {
+      return A[i];
+    }
+  }
+  if(dp[i][took_first][missed] > -1)
+    return dp[i][took_first][missed];
+
+  long long res = A[i] + rec(i + 1, took_first || (i == 0), 0);
+
+  if(missed == 0) {
+    res = min(res, rec(i + 1, took_first, missed + 1));
+  }
+
+  return dp[i][took_first][missed] = res;
+}
+
+void test_case() {  
+  cin >> N;
+  for(int i = 0; i < N; ++i)
+    cin >> A[i];
+
+  memset(dp, -1, sizeof(dp));
+  cout << rec(0, 0, 0) << '\n';
+} 
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  int tests = 1;
+  // cin >> tests;
+  while(tests--)
+    test_case();
+
+}
