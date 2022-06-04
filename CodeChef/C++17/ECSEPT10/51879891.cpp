2af11d2931a785a456aecebc9b0ff62ea7f9b0e4
Itish Agarwal <56088263+itish-agarwal@users.noreply.github.com>
2022-06-04 15:38:27 +0530

    Committing files

---
+++ CodeChef/C++17/ECSEPT10/51879891.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using namespace chrono;
+
+#ifndef ONLINE_JUDGE
+#define debug(x...) cout << "[" << #x << ": "; pr(x); cout << "]\n";
+#else
+#define debug(x...)
+#endif
+
+void pr(string x)  { cout << '\"' << x << '\"'; }
+void pr(char x)  { cout << '\'' << x << '\''; }
+void pr(long double x)  { cout << x; }
+void pr(long long x)  { cout << x; }
+void pr(double x)  { cout << x; }
+void pr(float x)  { cout << x; }
+void pr(long x)  { cout << x; }
+void pr(bool x)  { cout << x; }
+void pr(int x)  { cout << x; }
+
+template <class T, class V> void pr(const pair<T, V> &x);
+template <class T, class V> void pr(const map<T, V>& mp);
+template <class T, class... V> void pr(T t, V... v);
+template <class T> void pr(const multiset<T>& pq);
+template <class T> void pr(const vector<T>& v);
+template <class T> void pr(const set<T>& pq);
+
+template <class T, class V> void pr(const pair<T, V>& x) {
+  cout << "{"; pr(x.first); cout << ", "; pr(x.second); cout << "}";
+}
+template <class T, class V> void pr(const map<T, V>& mp) {
+  for(auto it = mp.begin(); it != mp.end(); ++it) { pr(*it); cout << " "; }
+}
+template <class T> void pr(const multiset<T>& pq) {
+  vector<T> values(pq.begin(), pq.end()); pr(values);
+}
+template <class T> void pr(const vector<T>& v) {
+  for(int i = 0; i < (int) v.size(); ++i) { pr(v[i]); cout << (i + 1 < (int) v.size() ? " " : ""); }
+}
+template <class T> void pr(const set<T>& pq) {
+  vector<T> values(pq.begin(), pq.end()); pr(values);
+}
+template <class T, class... V> void pr(T t, V... v) {
+  pr(t); if(sizeof...(v)) cout << " | ";
+  pr(v...);
+}
+//Global space begins
+
+vector<int> LPS(string S) {
+  int N = S.size();
+  //Assumes 1 indexed
+  vector<int> lps(N + 1, -1);
+  int i = 0, j = -1;
+  while(i < N) {
+    while(j != -1 && S[i] != S[j])
+      j = lps[j];
+    i++;
+    j++;
+    lps[i] = j;
+  }
+  return lps;
+}
+void test_case() {
+  string S;
+  cin >> S;
+
+  int N = S.size();
+  string rev = S;
+  reverse(rev.begin(), rev.end());
+
+  S += rev;
+  N += N;
+
+  vector<int> lps = LPS(S);
+  debug(lps);
+
+  cout << lps[N] << '\n';
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  int tests = 1;
+  // cin >> tests;
+
+  while(tests--) 
+    test_case();
+}
