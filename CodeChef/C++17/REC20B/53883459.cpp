3b1822961578ff65631dd1c735dc7bf722875413
Itish Agarwal <56088263+itish-agarwal@users.noreply.github.com>
2022-06-04 15:25:53 +0530

    Committing files

---
+++ CodeChef/C++17/REC20B/53883459.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using namespace chrono;
+
+#ifndef ONLINE_JUDGE
+#define debug(x...) cout << "[" << #x << ": "; pr(x); cout << "]\n";
+#else
+#define debug(x...)
+#endif
+
+void pr(string x)  { cout << '\"' << x << '\"'; }
+void pr(char x)  { cout << '\'' << x << '\''; }
+void pr(long long x)  { cout << x; }
+void pr(double x)  { cout << x; }
+void pr(bool x)  { cout << x; }
+void pr(int x)  { cout << x; }
+
+template <class T, class V> void pr(const pair<T, V> &x);
+template <class T, class V> void pr(const map<T, V>& mp);
+template <class T, class... V> void pr(T t, V... v);
+template <class T> void pr(const multiset<T>& pq);
+template <class T> void pr(const vector<T>& v);
+template <class T> void pr(const set<T>& pq);
+
+template <class T, class V> void pr(const pair<T, V>& x) {
+  cout << "{"; pr(x.first); cout << ", "; pr(x.second); cout << "}";
+}
+template <class T, class V> void pr(const map<T, V>& mp) {
+  for(auto it = mp.begin(); it != mp.end(); ++it) { pr(*it); cout << " "; }
+}
+template <class T> void pr(const multiset<T>& pq) {
+  vector<T> values(pq.begin(), pq.end()); pr(values);
+}
+template <class T> void pr(const vector<T>& v) {
+  for(int i = 0; i < (int) v.size(); ++i) { pr(v[i]); cout << (i + 1 < (int) v.size() ? " " : ""); }
+}
+template <class T> void pr(const set<T>& pq) {
+  vector<T> values(pq.begin(), pq.end()); pr(values);
+}
+template <class T, class... V> void pr(T t, V... v) {
+  pr(t); if(sizeof...(v)) cout << " | "; pr(v...);
+}
+//Global space begins
+const int INF = 2e9 + 7;
+
+int N;
+string S;
+string cpp = "c++";
+
+int dp[100009][5];
+
+int rec(int i, int j) {
+  if(i == N) {
+    return j == 3 ? INF : 0;
+  }
+
+  if(j == 3)
+    return INF;
+
+  if(dp[i][j] > -1)
+    return dp[i][j];
+
+  int res = 1 + rec(i + 1, j);
+  res = min(res, rec(i + 1, j + (S[i] == cpp[j])));
+
+  return dp[i][j] = res;
+}
+
+void test_case() {
+  cin >> N >> S;
+
+  memset(dp, -1, sizeof(dp));
+  cout << rec(0, 0) << '\n';
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  int tests = 1;
+  cin >> tests;
+
+  while(tests--) 
+    test_case();
+}
