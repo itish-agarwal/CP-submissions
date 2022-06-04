2f2e9acb189fb999813d91f23573c07cb5c80c1f
Itish Agarwal <56088263+itish-agarwal@users.noreply.github.com>
2022-06-04 15:36:16 +0530

    Committing files

---
+++ CodeChef/C++14/ZCO16002/51077167.cpp
@@ -0,0 +1,103 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using namespace chrono;
+
+#ifndef ONLINE_JUDGE
+#define debug(x...) cout << "[" << #x << ": "; pr(x); cout << "]\n";
+#else
+#define debug(x...)
+#endif
+
+void pr(string x)  { cout << '\"' << x << '\"'; }
+void pr(char x)  { cout << '\'' << x << '\''; }
+void pr(long double x)  { cout << x; }
+void pr(long long x)  { cout << x; }
+void pr(double x)  { cout << x; }
+void pr(float x)  { cout << x; }
+void pr(long x)  { cout << x; }
+void pr(bool x)  { cout << x; }
+void pr(int x)  { cout << x; }
+
+template <class T, class V> void pr(const pair<T, V> &x);
+template <class T, class V> void pr(const map<T, V>& mp);
+template <class T, class... V> void pr(T t, V... v);
+template <class T> void pr(const multiset<T>& pq);
+template <class T> void pr(const vector<T>& v);
+template <class T> void pr(const set<T>& pq);
+
+template <class T, class V> void pr(const pair<T, V>& x) {
+  cout << "{"; pr(x.first); cout << ", "; pr(x.second); cout << "}";
+}
+template <class T, class V> void pr(const map<T, V>& mp) {
+  for(auto it = mp.begin(); it != mp.end(); ++it) {
+    pr(*it); auto copy = it;
+    if(++copy != mp.end()) 
+      cout << " ";
+  }
+}
+template <class T> void pr(const multiset<T>& pq) {
+  vector<T> values;
+  for(auto& value : pq) 
+    values.push_back(value);
+
+  pr(values);
+}
+template <class T> void pr(const vector<T>& v) {
+  for(int i = 0; i < (int) v.size(); ++i) {
+    pr(v[i]); cout << (i + 1 < (int) v.size() ? " " : "");
+  }
+}
+template <class T> void pr(const set<T>& pq) {
+  vector<T> values;
+  for(auto value : pq)
+    values.push_back(value);
+
+  pr(values);
+}
+template <class T, class... V> void pr(T t, V... v) {
+  pr(t); if(sizeof...(v)) cout << " | ";
+  pr(v...);
+}
+//Global space begins
+
+void test_case() {
+  int N;
+  cin >> N;
+
+  vector<int> A(N);
+  for(auto &a : A)
+    cin >> a;
+
+  sort(A.begin(), A.end());
+
+  int MAX_N = A.back();
+  debug(MAX_N);
+
+  vector<vector<int>> dp(N + 3, vector<int> (MAX_N + 3));
+  
+  for(int i = 0; i < N; ++i) {
+    for(int j = 0; j < i; ++j) {
+      int d = A[i] - A[j];
+      dp[i][d] = max(2, dp[j][d] + 1);
+    }
+  }
+
+  int ans = 0;
+  for(int i = 0; i <= N; ++i)
+    for(int j = 0; j <= MAX_N; ++j)
+      ans = max(ans, dp[i][j]);
+
+  cout << ans << '\n';
+} 
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  int tests = 1;
+  // cin >> tests;
+  while(tests--)
+    test_case();
+
+}
