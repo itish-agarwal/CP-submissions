9fc27f2259d1856eb96027e121f4b447ce7e4800
Itish Agarwal <56088263+itish-agarwal@users.noreply.github.com>
2022-06-04 15:45:23 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1671C | Dolce Vita/157413387.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+using namespace chrono;
+ 
+#ifndef ONLINE_JUDGE
+#define debug(x...) cout << "[" << #x << ": "; pr(x); cout << "]\n";
+#else
+#define debug(x...)
+#endif
+ 
+void pr(string x)  { cout << '\"' << x << '\"'; }
+void pr(char x)  { cout << '\'' << x << '\''; }
+void pr(long long x)  { cout << x; }
+void pr(double x)  { cout << x; }
+void pr(bool x)  { cout << x; }
+void pr(int x)  { cout << x; }
+ 
+template <class T, class V> void pr(const pair<T, V> &x);
+template <class T, class V> void pr(const map<T, V>& mp);
+template <class T, class... V> void pr(T t, V... v);
+template <class T> void pr(const multiset<T>& pq);
+template <class T> void pr(const vector<T>& v);
+template <class T> void pr(const set<T>& pq);
+ 
+template <class T, class V> void pr(const pair<T, V>& x) {
+   cout << "{"; pr(x.first); cout << ", "; pr(x.second); cout << "}";
+}
+template <class T, class V> void pr(const map<T, V>& mp) {
+   for (auto it = mp.begin(); it != mp.end(); ++it) { pr(*it); cout << " "; }
+}
+template <class T> void pr(const multiset<T>& pq) {
+   vector<T> values(pq.begin(), pq.end()); pr(values);
+}
+template <class T> void pr(const vector<T>& v) {
+   for (int i = 0; i < (int) v.size(); ++i) { pr(v[i]); cout << (i + 1 < (int) v.size() ? " " : ""); }
+}
+template <class T> void pr(const set<T>& pq) {
+   vector<T> values(pq.begin(), pq.end()); pr(values);
+}
+template <class T, class... V> void pr(T t, V... v) {
+   pr(t); if(sizeof...(v)) cout << " | "; pr(v...);
+}
+//Global space begins
+ 
+void test_case() {
+   int N, X;
+   cin >> N >> X;
+ 
+   vector<int64_t> A(N);
+   for (auto &a : A)
+      cin >> a;
+ 
+   sort (A.begin(), A.end());
+ 
+   vector<int64_t> preffix_sum = A;
+   for (int i = 1; i < N; ++i)
+      preffix_sum[i] += preffix_sum[i-1];
+ 
+   int64_t ans = 0;
+   
+   for (int i = 0; i < N; ++i) {
+      int lo = 0, hi = 1e9, max_days = -1;
+      while (lo <= hi) {
+         int mid = (lo + hi) / 2;
+         if (preffix_sum[i] + (int64_t) mid * (i + 1) <= X) {
+            max_days = mid;
+            lo = mid + 1;
+         }
+         else {
+            hi = mid - 1;
+         }
+      }
+      ans += (int64_t) max_days + 1;
+   }
+ 
+   cout << ans << '\n';
+}
+ 
+int main() {
+   ios::sync_with_stdio(false);
+   cin.tie(0);
+ 
+   int tests = 1;
+   cin >> tests;
+ 
+   while(tests--)
+      test_case();
+}
