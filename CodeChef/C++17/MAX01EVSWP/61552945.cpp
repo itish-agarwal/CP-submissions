19b0df81712d4eaab0ffdadbe7f32fb9f5f38189
Itish Agarwal <56088263+itish-agarwal@users.noreply.github.com>
2022-06-04 15:37:08 +0530

    Committing files

---
+++ CodeChef/C++17/MAX01EVSWP/61552945.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+#define debug(x...) cout << "[" << #x << ": "; pr(x); cout << "]\n";
+#else
+#define debug(x...)
+#endif
+
+void pr(string x)  { cout << '\"' << x << '\"'; }
+void pr(long long x)  { cout << x; }
+void pr(bool x)  { cout << x; }
+void pr(int x)  { cout << x; }
+
+template <class T, class V> void pr(const map<T, V>& mp);
+template <class T, class... V> void pr(T t, V... v);
+template <class T> void pr(const multiset<T>& pq);
+template <class T> void pr(const vector<T>& v);
+
+template <class T, class V> void pr(const map<T, V>& mp) {
+   for (auto it = mp.begin(); it != mp.end(); ++it) { pr(*it); cout << " "; }
+}
+template <class T> void pr(const multiset<T>& pq) {
+   vector<T> values(pq.begin(), pq.end()); pr(values);
+}
+template <class T> void pr(const vector<T>& v) {
+   for (int i = 0; i < (int) v.size(); ++i) { pr(v[i]); cout << (i + 1 < (int) v.size() ? " " : ""); }
+}
+template <class T, class... V> void pr(T t, V... v) {
+   pr(t); if(sizeof...(v)) cout << " | "; pr(v...);
+}
+//Global space begins
+
+void test_case() {
+   int N;
+   string S;
+   cin >> N >> S;
+
+   vector<int> C0(2), C1(2);
+   for (int i = 0; i < N; ++i)
+      if (i % 2) {
+         C1[S[i]-'0'] += 1;
+      } else {
+         C0[S[i]-'0'] += 1;
+      }
+
+   int ans = 0;
+   for (int i = 0; i + 1 < N; ++i) {
+      if (i % 2) {
+         if (C1[0] > 0 && C0[1] > 0) {
+            ans += 1;
+            i += 1;
+            C1[0]--;
+            C0[1]--;
+         }
+      } else {
+         if (C0[0] > 0 && C1[1] > 0) {
+            ans += 1;
+            i += 1;
+            C0[0]--;
+            C1[1]--;
+         }
+      }
+   }
+
+   cout << ans << '\n';
+}
+
+int main() {
+   ios::sync_with_stdio(false);
+   cin.tie(0);
+
+   int tt = 1;
+   cin >> tt;
+
+   while(tt--)
+      test_case();
+}
