3a239f081d05bc1f171ac9479397c54cd10d4484
Itish Agarwal <56088263+itish-agarwal@users.noreply.github.com>
2022-06-04 15:44:35 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1670D | Very Suspicious/157592283.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+using namespace chrono;
+ 
+#ifndef ONLINE_JUDGE
+#define debug(x...) cout << "[" << #x << ": "; pr(x); cout << "]\n";
+#else
+#define debug(x...)
+#endif
+ 
+void pr(string x)  { cout << '\"' << x << '\"'; }
+void pr(char x)  { cout << '\'' << x << '\''; }
+void pr(long long x)  { cout << x; }
+void pr(double x)  { cout << x; }
+void pr(bool x)  { cout << x; }
+void pr(int x)  { cout << x; }
+ 
+template <class T, class V> void pr(const pair<T, V> &x);
+template <class T, class V> void pr(const map<T, V>& mp);
+template <class T, class... V> void pr(T t, V... v);
+template <class T> void pr(const multiset<T>& pq);
+template <class T> void pr(const vector<T>& v);
+template <class T> void pr(const set<T>& pq);
+ 
+template <class T, class V> void pr(const pair<T, V>& x) {
+   cout << "{"; pr(x.first); cout << ", "; pr(x.second); cout << "}";
+}
+template <class T, class V> void pr(const map<T, V>& mp) {
+   for (auto it = mp.begin(); it != mp.end(); ++it) { pr(*it); cout << " "; }
+}
+template <class T> void pr(const multiset<T>& pq) {
+   vector<T> values(pq.begin(), pq.end()); pr(values);
+}
+template <class T> void pr(const vector<T>& v) {
+   for (int i = 0; i < (int) v.size(); ++i) { pr(v[i]); cout << (i + 1 < (int) v.size() ? " " : ""); }
+}
+template <class T> void pr(const set<T>& pq) {
+   vector<T> values(pq.begin(), pq.end()); pr(values);
+}
+template <class T, class... V> void pr(T t, V... v) {
+   pr(t); if(sizeof...(v)) cout << " | "; pr(v...);
+}
+//Global space begins
+const int nax = 50000 + 1;
+vector<int64_t> dp(nax);
+ 
+int main() {
+   ios::sync_with_stdio(false);
+   cin.tie(0);
+ 
+   dp[2] = 2;
+   dp[3] = 6;
+ 
+   int64_t d = 4;
+   for (int i = 4; i < nax; ++i) {
+      dp[i] = dp[i-1] + d;
+      d += 2;
+      if (i % 3 == 0)
+         d -= 2;
+   }
+ 
+   int tt;
+   cin >> tt;
+   for (int t = 0; t < tt; ++t) {
+      int64_t N;
+      cin >> N;
+ 
+      cout << lower_bound (dp.begin(), dp.end(), N) - dp.begin() << '\n';
+   }
+}
