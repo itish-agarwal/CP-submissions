9a40bff2054fe4933de0c5089a96ce0bc2546531
Itish Agarwal <56088263+itish-agarwal@users.noreply.github.com>
2022-06-04 15:27:16 +0530

    Committing files

---
+++ CodeChef/C++17/PRTY_GAME/52094262.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using namespace chrono;
+
+#ifndef ONLINE_JUDGE
+#define debug(x...) cout << "[" << #x << ": "; pr(x); cout << "]\n";
+#else
+#define debug(x...)
+#endif
+
+void pr(string x)  { cout << '\"' << x << '\"'; }
+void pr(char x)  { cout << '\'' << x << '\''; }
+void pr(long double x)  { cout << x; }
+void pr(long long x)  { cout << x; }
+void pr(double x)  { cout << x; }
+void pr(float x)  { cout << x; }
+void pr(long x)  { cout << x; }
+void pr(bool x)  { cout << x; }
+void pr(int x)  { cout << x; }
+
+template <class T, class V> void pr(const pair<T, V> &x);
+template <class T, class V> void pr(const map<T, V>& mp);
+template <class T, class... V> void pr(T t, V... v);
+template <class T> void pr(const multiset<T>& pq);
+template <class T> void pr(const vector<T>& v);
+template <class T> void pr(const set<T>& pq);
+
+template <class T, class V> void pr(const pair<T, V>& x) {
+  cout << "{"; pr(x.first); cout << ", "; pr(x.second); cout << "}";
+}
+template <class T, class V> void pr(const map<T, V>& mp) {
+  for(auto it = mp.begin(); it != mp.end(); ++it) { pr(*it); cout << " "; }
+}
+template <class T> void pr(const multiset<T>& pq) {
+  vector<T> values(pq.begin(), pq.end()); pr(values);
+}
+template <class T> void pr(const vector<T>& v) {
+  for(int i = 0; i < (int) v.size(); ++i) { pr(v[i]); cout << (i + 1 < (int) v.size() ? " " : ""); }
+}
+template <class T> void pr(const set<T>& pq) {
+  vector<T> values(pq.begin(), pq.end()); pr(values);
+}
+template <class T, class... V> void pr(T t, V... v) {
+  pr(t); if(sizeof...(v)) cout << " | ";
+  pr(v...);
+}
+//Global space begins
+
+void test_case() {
+  int N, K;
+  cin >> N >> K;
+
+  vector<int> q(N);
+  iota(q.begin(), q.end(), 0);
+
+  int L = 0, R = N - 1, iter = 0;
+
+  while(R - L + 1 > K) {
+    if(iter % 2 == 0) {
+      L += K;
+    }
+    else {
+      R -= K;
+    }
+    // cout << L << " " << R << endl;
+    iter++;
+  }
+
+  // cout << L << " " << R << endl;
+  cout << (iter % 2 == 0 ? R + 1 : L + 1) << '\n';
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  int tests = 1;
+  cin >> tests;
+
+  while(tests--) 
+    test_case();
+}
