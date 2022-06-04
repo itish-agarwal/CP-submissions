481b3a6c54268f1eddd5966ef425f76b9d2a960d
Itish Agarwal <56088263+itish-agarwal@users.noreply.github.com>
2022-06-04 15:54:18 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1562A | The Miracle and the Sleeper/127060220.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+ 
+  int T;
+  cin >> T;
+ 
+  while(T--) {
+    int l, r;
+    cin >> l >> r;
+ 
+    int b = max(l, r/2 + 1);
+ 
+    cout << r % b << '\n';
+  }
+}
