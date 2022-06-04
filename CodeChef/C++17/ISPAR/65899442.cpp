eb522762bf915aa7c175ab100c1a7b2a164b7c0d
Itish Agarwal <56088263+itish-agarwal@users.noreply.github.com>
2022-06-04 15:25:18 +0530

    Committing files

---
+++ CodeChef/C++17/ISPAR/65899442.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using namespace chrono;
+
+#ifndef ONLINE_JUDGE
+#define debug(x...) cout << "[" << #x << ": "; pr(x); cout << "]\n";
+#else
+#define debug(x...)
+#endif
+
+void pr(string x)  { cout << '\"' << x << '\"'; }
+void pr(char x)  { cout << '\'' << x << '\''; }
+void pr(long long x)  { cout << x; }
+void pr(double x)  { cout << x; }
+void pr(bool x)  { cout << x; }
+void pr(int x)  { cout << x; }
+
+template <class T, class V> void pr(const pair<T, V> &x);
+template <class T, class V> void pr(const map<T, V>& mp);
+template <class T, class... V> void pr(T t, V... v);
+template <class T> void pr(const multiset<T>& pq);
+template <class T> void pr(const vector<T>& v);
+template <class T> void pr(const set<T>& pq);
+
+template <class T, class V> void pr(const pair<T, V>& x) {
+   cout << "{"; pr(x.first); cout << ", "; pr(x.second); cout << "}";
+}
+template <class T, class V> void pr(const map<T, V>& mp) {
+   for (auto it = mp.begin(); it != mp.end(); ++it) { pr(*it); cout << " "; }
+}
+template <class T> void pr(const multiset<T>& pq) {
+   vector<T> values(pq.begin(), pq.end()); pr(values);
+}
+template <class T> void pr(const vector<T>& v) {
+   for (int i = 0; i < (int) v.size(); ++i) { pr(v[i]); cout << (i + 1 < (int) v.size() ? " " : ""); }
+}
+template <class T> void pr(const set<T>& pq) {
+   vector<T> values(pq.begin(), pq.end()); pr(values);
+}
+template <class T, class... V> void pr(T t, V... v) {
+   pr(t); if(sizeof...(v)) cout << " | "; pr(v...);
+}
+//Global space begins
+
+void test_case() {
+   int64_t N, K;
+   cin >> N >> K;
+   
+   int ans = 0;
+   if (K == 1) {
+      ans = N % 2;
+   }
+   else if (K == 2) {
+      ans = 1;
+   }
+
+   cout << (ans ? "ODD" : "EVEN") << '\n';
+}
+
+int main() {
+   ios::sync_with_stdio(false);
+   cin.tie(0);
+
+   int tests = 1;
+   cin >> tests;
+
+   while(tests--)
+      test_case();
+}
