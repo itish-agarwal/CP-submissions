2a13d7c6985ca92cdeb4c0b9b9c9df3e02ffb42c
Itish Agarwal <56088263+itish-agarwal@users.noreply.github.com>
2022-06-04 15:27:27 +0530

    Committing files

---
+++ CodeChef/C++17/COG2103/51511810.cpp
@@ -0,0 +1,107 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using namespace chrono;
+
+#ifndef ONLINE_JUDGE
+#define debug(x...) cout << "[" << #x << ": "; pr(x); cout << "]\n";
+#else
+#define debug(x...)
+#endif
+
+void pr(string x)  { cout << '\"' << x << '\"'; }
+void pr(char x)  { cout << '\'' << x << '\''; }
+void pr(long double x)  { cout << x; }
+void pr(long long x)  { cout << x; }
+void pr(double x)  { cout << x; }
+void pr(float x)  { cout << x; }
+void pr(long x)  { cout << x; }
+void pr(bool x)  { cout << x; }
+void pr(int x)  { cout << x; }
+
+template <class T, class V> void pr(const pair<T, V> &x);
+template <class T, class V> void pr(const map<T, V>& mp);
+template <class T, class... V> void pr(T t, V... v);
+template <class T> void pr(const multiset<T>& pq);
+template <class T> void pr(const vector<T>& v);
+template <class T> void pr(const set<T>& pq);
+
+template <class T, class V> void pr(const pair<T, V>& x) {
+  cout << "{"; pr(x.first); cout << ", "; pr(x.second); cout << "}";
+}
+template <class T, class V> void pr(const map<T, V>& mp) {
+  for(auto it = mp.begin(); it != mp.end(); ++it) {
+    pr(*it); auto copy = it;
+    if(++copy != mp.end()) 
+      cout << " ";
+  }
+}
+template <class T> void pr(const multiset<T>& pq) {
+  vector<T> values;
+  for(auto& value : pq) 
+    values.push_back(value);
+
+  pr(values);
+}
+template <class T> void pr(const vector<T>& v) {
+  for(int i = 0; i < (int) v.size(); ++i) {
+    pr(v[i]); cout << (i + 1 < (int) v.size() ? " " : "");
+  }
+}
+template <class T> void pr(const set<T>& pq) {
+  vector<T> values;
+  for(auto value : pq)
+    values.push_back(value);
+
+  pr(values);
+}
+template <class T, class... V> void pr(T t, V... v) {
+  pr(t); if(sizeof...(v)) cout << " | ";
+  pr(v...);
+}
+//Global space begins
+
+const long long mod = 1e9 + 7;
+
+long long power(long long a, long long b) {
+  if(b == 0) 
+    return 1;
+ 
+  long long res = power(a, b / 2);
+  res = (res * res) % mod;
+  if(b % 2) 
+    res = (res * a) % mod;
+ 
+  return res;
+}
+
+void test_case() {
+  long long N;
+  cin >> N;
+
+  if(N % 2) {
+    long long x = N / 2;
+    cout << ((N % mod) * (x % mod)) % mod << '\n';
+  }
+
+  else {
+    long long ans = ((N % mod) * (N % mod)) % mod;
+    ans = (ans * power(2, mod - 2)) % mod;
+
+    long long sub = N % mod;
+    sub = (sub * power(2, mod - 2)) % mod;
+
+    cout << (ans - sub + mod) % mod << '\n';
+  } 
+} 
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  int tests = 1;
+  cin >> tests;
+  while(tests--)
+    test_case();
+
+}   
