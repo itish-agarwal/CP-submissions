90f331a0e49d62eb97d5160f73d6a1e06a79df77
Itish Agarwal <56088263+itish-agarwal@users.noreply.github.com>
2022-06-04 15:44:10 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1684D | Traps/157703374.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+using namespace chrono;
+ 
+#ifndef ONLINE_JUDGE
+#define debug(x...) cout << "[" << #x << ": "; pr(x); cout << "]\n";
+#else
+#define debug(x...)
+#endif
+ 
+void pr(string x)  { cout << '\"' << x << '\"'; }
+void pr(char x)  { cout << '\'' << x << '\''; }
+void pr(long long x)  { cout << x; }
+void pr(double x)  { cout << x; }
+void pr(bool x)  { cout << x; }
+void pr(int x)  { cout << x; }
+ 
+template <class T, class V> void pr(const pair<T, V> &x);
+template <class T, class V> void pr(const map<T, V>& mp);
+template <class T, class... V> void pr(T t, V... v);
+template <class T> void pr(const multiset<T>& pq);
+template <class T> void pr(const vector<T>& v);
+template <class T> void pr(const set<T>& pq);
+ 
+template <class T, class V> void pr(const pair<T, V>& x) {
+   cout << "{"; pr(x.first); cout << ", "; pr(x.second); cout << "}";
+}
+template <class T, class V> void pr(const map<T, V>& mp) {
+   for (auto it = mp.begin(); it != mp.end(); ++it) { pr(*it); cout << " "; }
+}
+template <class T> void pr(const multiset<T>& pq) {
+   vector<T> values(pq.begin(), pq.end()); pr(values);
+}
+template <class T> void pr(const vector<T>& v) {
+   for (int i = 0; i < (int) v.size(); ++i) { pr(v[i]); cout << (i + 1 < (int) v.size() ? " " : ""); }
+}
+template <class T> void pr(const set<T>& pq) {
+   vector<T> values(pq.begin(), pq.end()); pr(values);
+}
+template <class T, class... V> void pr(T t, V... v) {
+   pr(t); if(sizeof...(v)) cout << " | "; pr(v...);
+}
+//Global space begins
+ 
+void test_case() {
+   int N, K;
+   cin >> N >> K;
+ 
+   vector<int> A(N);
+   for (auto &a : A)
+      cin >> a;
+ 
+   vector<int> order(N);
+   iota (order.begin(), order.end(), 0);
+   sort (order.begin(), order.end(), [&] (int i, int j) {
+      return N - i -1 - A[i] < N - j - 1 - A[j];
+   });
+ 
+   vector<int> jump(N);
+   for (int i = 0; i < K; ++i)
+      jump[order[i]] = 1;
+   
+   int64_t ans = 0, cnt = 0;
+   for (int i = 0; i < N; ++i) {
+      if (jump[i])
+         cnt += 1;
+      else
+         ans += A[i] + cnt;
+   }
+ 
+   cout << ans << '\n';
+}
+ 
+int main() {
+   ios::sync_with_stdio(false);
+   cin.tie(0);
+ 
+   int tests = 1;
+   cin >> tests;
+ 
+   while(tests--)
+      test_case();
+}
