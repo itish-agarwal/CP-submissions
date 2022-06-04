3af277e33b52e8280227029f2bd6e57726e166d6
Itish Agarwal <56088263+itish-agarwal@users.noreply.github.com>
2022-06-04 15:50:22 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1589B | Coloring Rectangles/136137214.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+using namespace chrono;
+ 
+#ifndef ONLINE_JUDGE
+#define debug(x...) cout << "[" << #x << ": "; pr(x); cout << "]\n";
+#else
+#define debug(x...)
+#endif
+ 
+void pr(string x)  { cout << '\"' << x << '\"'; }
+void pr(char x)  { cout << '\'' << x << '\''; }
+void pr(long long x)  { cout << x; }
+void pr(double x)  { cout << x; }
+void pr(bool x)  { cout << x; }
+void pr(int x)  { cout << x; }
+ 
+template <class T, class V> void pr(const pair<T, V> &x);
+template <class T, class V> void pr(const map<T, V>& mp);
+template <class T, class... V> void pr(T t, V... v);
+template <class T> void pr(const multiset<T>& pq);
+template <class T> void pr(const vector<T>& v);
+template <class T> void pr(const set<T>& pq);
+ 
+template <class T, class V> void pr(const pair<T, V>& x) {
+  cout << "{"; pr(x.first); cout << ", "; pr(x.second); cout << "}";
+}
+template <class T, class V> void pr(const map<T, V>& mp) {
+  for(auto it = mp.begin(); it != mp.end(); ++it) { pr(*it); cout << " "; }
+}
+template <class T> void pr(const multiset<T>& pq) {
+  vector<T> values(pq.begin(), pq.end()); pr(values);
+}
+template <class T> void pr(const vector<T>& v) {
+  for(int i = 0; i < (int) v.size(); ++i) { pr(v[i]); cout << (i + 1 < (int) v.size() ? " " : ""); }
+}
+template <class T> void pr(const set<T>& pq) {
+  vector<T> values(pq.begin(), pq.end()); pr(values);
+}
+template <class T, class... V> void pr(T t, V... v) {
+  pr(t); if(sizeof...(v)) cout << " | "; pr(v...);
+}
+//Global space begins
+ 
+int solve(int N, int M) {
+  int res = N * (M / 3);
+ 
+  if(M % 3 == 1) {
+    res += N / 3;
+    if(N % 3 != 0)
+      res++;
+  }
+ 
+  else if(M % 3 == 2) {
+    res += 2 * (N / 3) + N % 3;
+  }
+ 
+  return res;
+}
+void test_case() {
+  int N, M;
+  cin >> N >> M;
+ 
+  cout << min(solve(N, M), solve(M, N)) << '\n';
+}
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+ 
+  int tests = 1;
+  cin >> tests;
+ 
+  while(tests--) 
+    test_case();
+}
