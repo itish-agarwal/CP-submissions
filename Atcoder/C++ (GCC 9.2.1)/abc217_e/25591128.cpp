808f2ce6e75fb0520f108c1c1257565cd2670dd9
Itish Agarwal <56088263+itish-agarwal@users.noreply.github.com>
2022-06-04 15:19:14 +0530

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc217_e/25591128.cpp
@@ -0,0 +1,112 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using namespace chrono;
+
+#ifndef ONLINE_JUDGE
+#define debug(x...) cout << "[" << #x << ": "; pr(x); cout << "]\n";
+#else
+#define debug(x...)
+#endif
+
+void pr(string x)  { cout << '\"' << x << '\"'; }
+void pr(char x)  { cout << '\'' << x << '\''; }
+void pr(long double x)  { cout << x; }
+void pr(long long x)  { cout << x; }
+void pr(double x)  { cout << x; }
+void pr(float x)  { cout << x; }
+void pr(long x)  { cout << x; }
+void pr(bool x)  { cout << x; }
+void pr(int x)  { cout << x; }
+
+template <class T, class V> void pr(const pair<T, V> &x);
+template <class T, class V> void pr(const map<T, V>& mp);
+template <class T, class... V> void pr(T t, V... v);
+template <class T> void pr(const multiset<T>& pq);
+template <class T> void pr(const vector<T>& v);
+template <class T> void pr(const set<T>& pq);
+
+template <class T, class V> void pr(const pair<T, V>& x) {
+  cout << "{"; pr(x.first); cout << ", "; pr(x.second); cout << "}";
+}
+template <class T, class V> void pr(const map<T, V>& mp) {
+  for(auto it = mp.begin(); it != mp.end(); ++it) {
+    pr(*it); auto copy = it;
+    if(++copy != mp.end()) 
+      cout << " ";
+  }
+}
+template <class T> void pr(const multiset<T>& pq) {
+  vector<T> values;
+  for(auto& value : pq) 
+    values.push_back(value);
+
+  pr(values);
+}
+template <class T> void pr(const vector<T>& v) {
+  for(int i = 0; i < (int) v.size(); ++i) {
+    pr(v[i]); cout << (i + 1 < (int) v.size() ? " " : "");
+  }
+}
+template <class T> void pr(const set<T>& pq) {
+  vector<T> values;
+  for(auto value : pq)
+    values.push_back(value);
+
+  pr(values);
+}
+template <class T, class... V> void pr(T t, V... v) {
+  pr(t); if(sizeof...(v)) cout << " | ";
+  pr(v...);
+}
+//Global space begins
+
+void test_case() {
+  int Q;
+  cin >> Q;
+
+  multiset<int> pq;
+  vector<int> sequence;
+
+  int iter = 0;
+
+  for(int q = 0; q < Q; ++q) {
+    int tt;
+    cin >> tt;
+    if(tt == 1) {
+      int x;
+      cin >> x;
+      sequence.emplace_back(x);
+    }
+    else if(tt == 2) {
+      if(!pq.empty()) {
+        int x = *pq.begin();
+        pq.erase(pq.find(x));
+        cout << x << '\n';
+      }
+
+      else {
+        cout << sequence[iter++] << '\n';
+      }
+    }
+
+    else {
+      for(int i = iter; i < (int) sequence.size(); ++i)
+        pq.insert(sequence[i]);
+
+      sequence.clear();
+      iter = 0;
+    }
+  }
+} 
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  int tests = 1;
+  // cin >> tests;
+  while(tests--)
+    test_case();
+
+}
