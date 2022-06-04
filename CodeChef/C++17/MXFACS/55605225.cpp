4b4e34e4d6c3b9cef1251159d7d105156c5fc127
Itish Agarwal <56088263+itish-agarwal@users.noreply.github.com>
2022-06-04 15:25:40 +0530

    Committing files

---
+++ CodeChef/C++17/MXFACS/55605225.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using namespace chrono;
+
+#ifndef ONLINE_JUDGE
+#define debug(x...) cout << "[" << #x << ": "; pr(x); cout << "]\n";
+#else
+#define debug(x...)
+#endif
+
+void pr(string x)  { cout << '\"' << x << '\"'; }
+void pr(char x)  { cout << '\'' << x << '\''; }
+void pr(long long x)  { cout << x; }
+void pr(double x)  { cout << x; }
+void pr(bool x)  { cout << x; }
+void pr(int x)  { cout << x; }
+
+template <class T, class V> void pr(const pair<T, V> &x);
+template <class T, class V> void pr(const map<T, V>& mp);
+template <class T, class... V> void pr(T t, V... v);
+template <class T> void pr(const multiset<T>& pq);
+template <class T> void pr(const vector<T>& v);
+template <class T> void pr(const set<T>& pq);
+
+template <class T, class V> void pr(const pair<T, V>& x) {
+   cout << "{"; pr(x.first); cout << ", "; pr(x.second); cout << "}";
+}
+template <class T, class V> void pr(const map<T, V>& mp) {
+   for (auto it = mp.begin(); it != mp.end(); ++it) { pr(*it); cout << " "; }
+}
+template <class T> void pr(const multiset<T>& pq) {
+   vector<T> values(pq.begin(), pq.end()); pr(values);
+}
+template <class T> void pr(const vector<T>& v) {
+   for (int i = 0; i < (int) v.size(); ++i) { pr(v[i]); cout << (i + 1 < (int) v.size() ? " " : ""); }
+}
+template <class T> void pr(const set<T>& pq) {
+   vector<T> values(pq.begin(), pq.end()); pr(values);
+}
+template <class T, class... V> void pr(T t, V... v) {
+   pr(t); if(sizeof...(v)) cout << " | "; pr(v...);
+}
+//Global space begins
+
+vector<pair<int, int>> get_factors(int n) {
+   vector<pair<int, int>> res;
+   for (int x = 2; x * x <= n; ++x) 
+      if (n % x == 0) {
+         int t = 0;
+         while(n % x == 0) {
+            n /= x;
+            t++;
+         }
+         res.emplace_back(x, t);
+      }
+
+   if (n > 1)
+      res.emplace_back(n, 1);
+
+   return res;
+}
+
+void test_case() {
+   int N;
+   cin >> N;
+
+   auto factors = get_factors(N);
+   sort(factors.begin(), factors.end(), [&](const pair<int, int>& x, const pair<int, int>& y) {
+      if (x.second != y.second)
+         return x.second > y.second;
+
+      return x.first < y.first;
+   });
+
+   // debug(factors);
+
+   cout << factors[0].first << '\n';
+}
+
+int main() {
+   ios::sync_with_stdio(false);
+   cin.tie(0);
+
+   int tests = 1;
+   cin >> tests;
+
+   while(tests--)
+      test_case();
+}
