aa08f8e8bf28fce20e048a2d571aff882b651e41
Itish Agarwal <56088263+itish-agarwal@users.noreply.github.com>
2022-06-04 15:25:44 +0530

    Committing files

---
+++ CodeChef/C++17/CNTPEAK/55597679.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using namespace chrono;
+
+#ifndef ONLINE_JUDGE
+#define debug(x...) cout << "[" << #x << ": "; pr(x); cout << "]\n";
+#else
+#define debug(x...)
+#endif
+
+void pr(string x)  { cout << '\"' << x << '\"'; }
+void pr(char x)  { cout << '\'' << x << '\''; }
+void pr(long long x)  { cout << x; }
+void pr(double x)  { cout << x; }
+void pr(bool x)  { cout << x; }
+void pr(int x)  { cout << x; }
+
+template <class T, class V> void pr(const pair<T, V> &x);
+template <class T, class V> void pr(const map<T, V>& mp);
+template <class T, class... V> void pr(T t, V... v);
+template <class T> void pr(const multiset<T>& pq);
+template <class T> void pr(const vector<T>& v);
+template <class T> void pr(const set<T>& pq);
+
+template <class T, class V> void pr(const pair<T, V>& x) {
+   cout << "{"; pr(x.first); cout << ", "; pr(x.second); cout << "}";
+}
+template <class T, class V> void pr(const map<T, V>& mp) {
+   for (auto it = mp.begin(); it != mp.end(); ++it) { pr(*it); cout << " "; }
+}
+template <class T> void pr(const multiset<T>& pq) {
+   vector<T> values(pq.begin(), pq.end()); pr(values);
+}
+template <class T> void pr(const vector<T>& v) {
+   for (int i = 0; i < (int) v.size(); ++i) { pr(v[i]); cout << (i + 1 < (int) v.size() ? " " : ""); }
+}
+template <class T> void pr(const set<T>& pq) {
+   vector<T> values(pq.begin(), pq.end()); pr(values);
+}
+template <class T, class... V> void pr(T t, V... v) {
+   pr(t); if(sizeof...(v)) cout << " | "; pr(v...);
+}
+//Global space begins
+
+int pow_3(int n) {
+   if (n == 0)
+      return 1;
+   return 3 * pow_3(n - 1);
+}
+
+int peaks(const vector<int>& a) {
+   const int n = a.size();
+   int res = 0;
+   for (int i = 1; i < n - 1; ++i)
+      if ((a[i] > a[i-1] && a[i] > a[i+1]) || (a[i] < a[i-1] && a[i] < a[i+1]))
+         res++;
+
+   return res;
+}
+
+void test_case() {
+   int N;
+   cin >> N;
+
+   int ans = 0;
+   for (int temp = 0; temp < pow_3(N); ++temp) {
+      int x = temp;
+      vector<int> a;
+      for (int r = 0; r < N; ++r) {
+         a.push_back(x % 3);
+         x /= 3;
+      }
+
+      ans += peaks(a);
+   }
+
+   cout << ans << '\n';
+}
+
+int main() {
+   ios::sync_with_stdio(false);
+   cin.tie(0);
+
+   int tests = 1;
+   cin >> tests;
+
+   while(tests--)
+      test_case();
+}
