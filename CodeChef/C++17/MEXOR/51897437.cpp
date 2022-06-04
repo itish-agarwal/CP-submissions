7476822f7ee4dd5f957e7275ea0aff4004b893f1
Itish Agarwal <56088263+itish-agarwal@users.noreply.github.com>
2022-06-04 15:27:20 +0530

    Committing files

---
+++ CodeChef/C++17/MEXOR/51897437.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using namespace chrono;
+
+#ifndef ONLINE_JUDGE
+#define debug(x...) cout << "[" << #x << ": "; pr(x); cout << "]\n";
+#else
+#define debug(x...)
+#endif
+
+void pr(string x)  { cout << '\"' << x << '\"'; }
+void pr(char x)  { cout << '\'' << x << '\''; }
+void pr(long double x)  { cout << x; }
+void pr(long long x)  { cout << x; }
+void pr(double x)  { cout << x; }
+void pr(float x)  { cout << x; }
+void pr(long x)  { cout << x; }
+void pr(bool x)  { cout << x; }
+void pr(int x)  { cout << x; }
+
+template <class T, class V> void pr(const pair<T, V> &x);
+template <class T, class V> void pr(const map<T, V>& mp);
+template <class T, class... V> void pr(T t, V... v);
+template <class T> void pr(const multiset<T>& pq);
+template <class T> void pr(const vector<T>& v);
+template <class T> void pr(const set<T>& pq);
+
+template <class T, class V> void pr(const pair<T, V>& x) {
+  cout << "{"; pr(x.first); cout << ", "; pr(x.second); cout << "}";
+}
+template <class T, class V> void pr(const map<T, V>& mp) {
+  for(auto it = mp.begin(); it != mp.end(); ++it) { pr(*it); cout << " "; }
+}
+template <class T> void pr(const multiset<T>& pq) {
+  vector<T> values(pq.begin(), pq.end()); pr(values);
+}
+template <class T> void pr(const vector<T>& v) {
+  for(int i = 0; i < (int) v.size(); ++i) { pr(v[i]); cout << (i + 1 < (int) v.size() ? " " : ""); }
+}
+template <class T> void pr(const set<T>& pq) {
+  vector<T> values(pq.begin(), pq.end()); pr(values);
+}
+template <class T, class... V> void pr(T t, V... v) {
+  pr(t); if(sizeof...(v)) cout << " | ";
+  pr(v...);
+}
+//Global space begins
+using ll = long long;
+const ll INF = 2e15 + 9; 
+
+ll fun(ll n) {
+  ll res = 0;
+  for(int bit = 0; bit < 34; ++bit) {
+    ll mn = 1LL << bit;
+    if(mn <= n)
+      res += 1LL << bit;
+  }
+
+  return res;
+}
+void test_case() {
+  ll X;
+  cin >> X;
+
+  ll lo = 0, hi = INF, mex = 0;
+  while(lo <= hi) {
+    ll mid = (lo + hi) / 2;
+    ll OrUpto = fun(mid);
+    if(OrUpto <= X) {
+      lo = mid + 1;
+      mex = mid;
+    }
+    else {
+      hi = mid - 1;
+    }
+  } 
+
+  cout << mex + 1 << '\n';
+} 
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  int tests = 1;
+  cin >> tests;
+
+  while(tests--) 
+    test_case();
+}
