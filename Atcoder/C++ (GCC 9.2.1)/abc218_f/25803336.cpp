0b58c8b750411f163423ac6bb43c8814540c12d4
Itish Agarwal <56088263+itish-agarwal@users.noreply.github.com>
2022-06-04 15:19:02 +0530

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc218_f/25803336.cpp
@@ -0,0 +1,104 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using namespace chrono;
+
+#ifndef ONLINE_JUDGE
+#define debug(x...) cout << "[" << #x << ": "; pr(x); cout << "]\n";
+#else
+#define debug(x...)
+#endif
+
+void pr(string x)  { cout << '\"' << x << '\"'; }
+void pr(char x)  { cout << '\'' << x << '\''; }
+void pr(long double x)  { cout << x; }
+void pr(long long x)  { cout << x; }
+void pr(double x)  { cout << x; }
+void pr(float x)  { cout << x; }
+void pr(long x)  { cout << x; }
+void pr(bool x)  { cout << x; }
+void pr(int x)  { cout << x; }
+
+template <class T, class V> void pr(const pair<T, V> &x);
+template <class T, class V> void pr(const map<T, V>& mp);
+template <class T, class... V> void pr(T t, V... v);
+template <class T> void pr(const multiset<T>& pq);
+template <class T> void pr(const vector<T>& v);
+template <class T> void pr(const set<T>& pq);
+
+template <class T, class V> void pr(const pair<T, V>& x) {
+  cout << "{"; pr(x.first); cout << ", "; pr(x.second); cout << "}";
+}
+template <class T, class V> void pr(const map<T, V>& mp) {
+  for(auto it = mp.begin(); it != mp.end(); ++it) {
+    pr(*it); auto copy = it;
+    if(++copy != mp.end()) 
+      cout << " ";
+  }
+}
+template <class T> void pr(const multiset<T>& pq) {
+  vector<T> values;
+  for(auto& value : pq) 
+    values.push_back(value);
+
+  pr(values);
+}
+template <class T> void pr(const vector<T>& v) {
+  for(int i = 0; i < (int) v.size(); ++i) {
+    pr(v[i]); cout << (i + 1 < (int) v.size() ? " " : "");
+  }
+}
+template <class T> void pr(const set<T>& pq) {
+  vector<T> values;
+  for(auto value : pq)
+    values.push_back(value);
+
+  pr(values);
+}
+template <class T, class... V> void pr(T t, V... v) {
+  pr(t); if(sizeof...(v)) cout << " | ";
+  pr(v...);
+}
+//Global space begins
+
+void test_case() {
+  int N;
+  cin >> N;
+
+  vector<pair<int, int>> pts;
+  set<pair<int, int>> pq;
+  for(int i = 0; i < N; ++i) {
+    int x, y;
+    cin >> x >> y;  
+    pts.emplace_back(x, y);
+    pq.insert({x, y});
+  }
+
+  long long ans = 0;
+
+  sort(pts.begin(), pts.end());
+
+  for(int i = 0; i < N; ++i) {
+    for(int j = i + 1; j < N; ++j) {
+      int x1 = pts[i].first, y1 = pts[i].second, x2 = pts[j].first, y2 = pts[j].second;
+      if(x2 > x1 && y2 > y1) {
+        if(pq.find({x2, y1}) != pq.end() && pq.find({x1, y2}) != pq.end()) {
+          ans += 1;
+        }
+      }
+    }
+  }
+
+  cout << ans << '\n';
+} 
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  int tests = 1;
+  // cin >> tests;
+  while(tests--)
+    test_case();
+
+}
