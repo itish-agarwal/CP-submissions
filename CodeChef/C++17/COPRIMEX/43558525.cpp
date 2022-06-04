235b401f044fe138d0130d44695ebb104a18694e
Itish Agarwal <56088263+itish-agarwal@users.noreply.github.com>
2022-06-04 15:40:24 +0530

    Committing files

---
+++ CodeChef/C++17/COPRIMEX/43558525.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int tt;
+  cin >> tt;
+  while(tt--) {
+    int l, r;
+    cin >> l >> r;
+    int ans = 1e6 + 3;
+    cout << ans << '\n';
+  }
+}
