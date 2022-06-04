1ea6794596f20ffef12429de1e74e4c0be43c739
Itish Agarwal <56088263+itish-agarwal@users.noreply.github.com>
2022-06-04 15:27:14 +0530

    Committing files

---
+++ CodeChef/C++17/COMP_NET/52094503.cpp
@@ -0,0 +1,113 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using namespace chrono;
+
+#ifndef ONLINE_JUDGE
+#define debug(x...) cout << "[" << #x << ": "; pr(x); cout << "]\n";
+#else
+#define debug(x...)
+#endif
+
+void pr(string x)  { cout << '\"' << x << '\"'; }
+void pr(char x)  { cout << '\'' << x << '\''; }
+void pr(long double x)  { cout << x; }
+void pr(long long x)  { cout << x; }
+void pr(double x)  { cout << x; }
+void pr(float x)  { cout << x; }
+void pr(long x)  { cout << x; }
+void pr(bool x)  { cout << x; }
+void pr(int x)  { cout << x; }
+
+template <class T, class V> void pr(const pair<T, V> &x);
+template <class T, class V> void pr(const map<T, V>& mp);
+template <class T, class... V> void pr(T t, V... v);
+template <class T> void pr(const multiset<T>& pq);
+template <class T> void pr(const vector<T>& v);
+template <class T> void pr(const set<T>& pq);
+
+template <class T, class V> void pr(const pair<T, V>& x) {
+  cout << "{"; pr(x.first); cout << ", "; pr(x.second); cout << "}";
+}
+template <class T, class V> void pr(const map<T, V>& mp) {
+  for(auto it = mp.begin(); it != mp.end(); ++it) { pr(*it); cout << " "; }
+}
+template <class T> void pr(const multiset<T>& pq) {
+  vector<T> values(pq.begin(), pq.end()); pr(values);
+}
+template <class T> void pr(const vector<T>& v) {
+  for(int i = 0; i < (int) v.size(); ++i) { pr(v[i]); cout << (i + 1 < (int) v.size() ? " " : ""); }
+}
+template <class T> void pr(const set<T>& pq) {
+  vector<T> values(pq.begin(), pq.end()); pr(values);
+}
+template <class T, class... V> void pr(T t, V... v) {
+  pr(t); if(sizeof...(v)) cout << " | ";
+  pr(v...);
+}
+//Global space begins
+
+struct DSU {
+  vector<int> p, sz;
+  int components;
+
+  DSU(int n) {
+    p.resize(n);
+    sz.assign(n, 1);
+    components = n;
+    iota(p.begin(), p.end(), 0);
+  }
+  inline int get(int x) {
+    return x == p[x] ? x : p[x] = get(p[x]);
+  }
+
+  void unite(int x, int y) {
+    x = get(x), y = get(y);
+    if(x != y) {
+      if(sz[y] > sz[x]) 
+        swap(x, y);
+
+      sz[x] += sz[y];
+      components--;
+      p[y] = x;
+    }
+  }
+
+  inline bool belong(int x, int y) {
+    return get(x) == get(y);
+  }
+
+  inline int get_size(int x) {
+    return sz[get(x)];
+  }
+};
+
+void test_case() {
+  int N, M;
+  cin >> N >> M;
+
+  DSU d(N);
+  for(int i = 0; i < M; ++i) {
+    int a, b;
+    cin >> a >> b;
+    --a, --b;
+    d.unite(a, b);
+  }
+  int ans = 0;
+
+  for(int a = 0; a < N; ++a)
+    ans = max(ans, d.get_size(a));
+
+  cout << ans << '\n';
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  int tests = 1;
+  // cin >> tests;
+
+  while(tests--) 
+    test_case();
+}
