0daa4938b1a41e0ae9230c3f32d4d15406d487ae
Itish Agarwal <56088263+itish-agarwal@users.noreply.github.com>
2022-06-04 15:25:51 +0530

    Committing files

---
+++ CodeChef/C++17/PRDTPAIN/54094671.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using namespace chrono;
+
+#ifndef ONLINE_JUDGE
+#define debug(x...) cout << "[" << #x << ": "; pr(x); cout << "]\n";
+#else
+#define debug(x...)
+#endif
+
+void pr(string x)  { cout << '\"' << x << '\"'; }
+void pr(char x)  { cout << '\'' << x << '\''; }
+void pr(long long x)  { cout << x; }
+void pr(double x)  { cout << x; }
+void pr(bool x)  { cout << x; }
+void pr(int x)  { cout << x; }
+
+template <class T, class V> void pr(const pair<T, V> &x);
+template <class T, class V> void pr(const map<T, V>& mp);
+template <class T, class... V> void pr(T t, V... v);
+template <class T> void pr(const multiset<T>& pq);
+template <class T> void pr(const vector<T>& v);
+template <class T> void pr(const set<T>& pq);
+
+template <class T, class V> void pr(const pair<T, V>& x) {
+  cout << "{"; pr(x.first); cout << ", "; pr(x.second); cout << "}";
+}
+template <class T, class V> void pr(const map<T, V>& mp) {
+  for(auto it = mp.begin(); it != mp.end(); ++it) { pr(*it); cout << " "; }
+}
+template <class T> void pr(const multiset<T>& pq) {
+  vector<T> values(pq.begin(), pq.end()); pr(values);
+}
+template <class T> void pr(const vector<T>& v) {
+  for(int i = 0; i < (int) v.size(); ++i) { pr(v[i]); cout << (i + 1 < (int) v.size() ? " " : ""); }
+}
+template <class T> void pr(const set<T>& pq) {
+  vector<T> values(pq.begin(), pq.end()); pr(values);
+}
+template <class T, class... V> void pr(T t, V... v) {
+  pr(t); if(sizeof...(v)) cout << " | "; pr(v...);
+}
+//Global space begins
+
+void test_case() {
+  int N;
+  cin >> N;
+
+  vector<long long> A(N);
+  for(auto &a : A)
+    cin >> a;
+
+  //sorted
+  long long ans = 0;
+
+  for(int i = 0; i < N; ++i)
+    for(int j = i + 2; j < N; ++j) {
+      long long mid = (A[i] + A[j]) / 2;
+      int inl = lower_bound(A.begin(), A.end(), mid) - A.begin();
+      int inr = upper_bound(A.begin(), A.end(), mid) - A.begin() - 1;
+      long long weight = 0;
+      if(inl > i && inl < j)
+        weight = max(weight, (A[i] - A[inl]) * (A[inl] - A[j]));
+
+      if(inr > i && inr < j)
+        weight = max(weight, (A[i] - A[inr]) * (A[inr] - A[j]));
+
+      ans += weight;
+    }
+
+  cout << ans << '\n';
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  int tests = 1;
+  cin >> tests;
+
+  while(tests--) 
+    test_case();
+}
