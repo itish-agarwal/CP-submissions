fb27d78327239ea99a2406fecba06ef55126a327
Itish Agarwal <56088263+itish-agarwal@users.noreply.github.com>
2022-06-04 15:22:29 +0530

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc186_b/18863844.cpp
@@ -0,0 +1,28 @@
+
+#include <bits/stdc++.h>
+
+using namespace std;
+#define int int64_t
+
+ostream& operator << (ostream& stream, const vector<int>& v) {
+  for(int x : v) {
+    stream << x << " ";
+  }
+  return stream;
+}
+
+//ostream& operator << (ostream& stream, const int *a) {
+  //for(int i = 0; i < n; i++) {
+    //stream << *(a + i) <<  " ";
+  //}
+  //return stream;
+//}
+
+signed main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int a, b;
+  cin >> a >> b;
+  cout << a / b << '\n';
+  return 0;
+}
