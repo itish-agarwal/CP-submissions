0182901e821c2c5c48552e73922c2ec2b38d01c7
Itish Agarwal <56088263+itish-agarwal@users.noreply.github.com>
2022-06-04 15:34:41 +0530

    Committing files

---
+++ CodeChef/C++17/CYBV/40922261.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve() {
+  int n, k;
+  cin >> n >> k;
+  cout << k/n << '\n';
+  return;
+}
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int tt;
+  cin >> tt;
+  while(tt--) {
+    solve();
+  }
+}
+      
