8b4c164996cd3dedd55ff2f3e2399237130873d7
Itish Agarwal <56088263+itish-agarwal@users.noreply.github.com>
2022-06-04 15:50:59 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1586A | Windblume Ode/132321102.cpp
@@ -0,0 +1,97 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+using namespace chrono;
+ 
+#ifndef ONLINE_JUDGE
+#define debug(x...) cout << "[" << #x << ": "; pr(x); cout << "]\n";
+#else
+#define debug(x...)
+#endif
+ 
+void pr(string x)  { cout << '\"' << x << '\"'; }
+void pr(char x)  { cout << '\'' << x << '\''; }
+void pr(long double x)  { cout << x; }
+void pr(long long x)  { cout << x; }
+void pr(double x)  { cout << x; }
+void pr(float x)  { cout << x; }
+void pr(long x)  { cout << x; }
+void pr(bool x)  { cout << x; }
+void pr(int x)  { cout << x; }
+ 
+template <class T, class V> void pr(const pair<T, V> &x);
+template <class T, class V> void pr(const map<T, V>& mp);
+template <class T, class... V> void pr(T t, V... v);
+template <class T> void pr(const multiset<T>& pq);
+template <class T> void pr(const vector<T>& v);
+template <class T> void pr(const set<T>& pq);
+ 
+template <class T, class V> void pr(const pair<T, V>& x) {
+  cout << "{"; pr(x.first); cout << ", "; pr(x.second); cout << "}";
+}
+template <class T, class V> void pr(const map<T, V>& mp) {
+  for(auto it = mp.begin(); it != mp.end(); ++it) { pr(*it); cout << " "; }
+}
+template <class T> void pr(const multiset<T>& pq) {
+  vector<T> values(pq.begin(), pq.end()); pr(values);
+}
+template <class T> void pr(const vector<T>& v) {
+  for(int i = 0; i < (int) v.size(); ++i) { pr(v[i]); cout << (i + 1 < (int) v.size() ? " " : ""); }
+}
+template <class T> void pr(const set<T>& pq) {
+  vector<T> values(pq.begin(), pq.end()); pr(values);
+}
+template <class T, class... V> void pr(T t, V... v) {
+  pr(t); if(sizeof...(v)) cout << " | ";
+  pr(v...);
+}
+//Global space begins
+ 
+bool prime(int n) {
+  for(int i = 2; i*i <= n; i++) {
+    if(n%i == 0) {
+      return false;
+    }
+  }
+  return n >= 2;
+}
+ 
+void test_case() {
+  int N;
+  cin >> N;
+ 
+  vector<int> A(N);
+  for(auto &a : A)
+    cin >> a;
+ 
+  int sum = accumulate(A.begin(), A.end(), 0);
+  if(prime(sum)) {
+    int odd_index = -1;
+    for(int i = 0; i < N; ++i)
+      if(A[i] % 2)
+        odd_index = i;
+ 
+    cout << N - 1 << '\n';
+    for(int i = 0; i < N; ++i)
+      if(i != odd_index)
+        cout << i + 1 << " ";
+  }
+  else {
+    cout << N << '\n';
+    for(int i = 0; i < N; ++i)
+      cout << i + 1 << " ";
+  }
+ 
+  cout << '\n';
+}
+ 
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+ 
+  int tests = 1;
+  cin >> tests;
+ 
+  while(tests--) 
+    test_case();
+}
